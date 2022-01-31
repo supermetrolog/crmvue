ff95b8fe7f2671ad47040c243ef9c650dc06dbb0
Абдурашидов Тимур <tim-a@realtor.ru>
2022-01-31 14:09:51 +0300

    fix objects axios bugs

--- src/api/objects.js
+++ src/api/objects.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 import ErrorHandle from "./errors";
 import SuccessHandler from "./success";
+
+const pennylaneAxios = axios.create({
+    baseURL: 'https://pennylane.pro/api/v1/get/'
+});
 export default {
     async getCurrentStepObjects(objectsIdList) {
-        const BearerToken = axios.defaults.headers.common["Authorization"];
-        delete axios.defaults.headers.common["Authorization"];
-        const url = `https://pennylane.pro/api/v1/get/list/`;
+        const url = `list/`;
         const formdata = new FormData();
         const params = {
             offers_id: objectsIdList,
         };
         formdata.append("request", JSON.stringify(params));
         let data = false;
-        await axios
+        await pennylaneAxios
             .post(url, formdata)
             .then((Response) => {
                 data = SuccessHandler.getData(Response);
@@ -20,22 +22,16 @@ export default {
             })
             .catch((e) => ErrorHandle.setError(e));
 
-        axios.defaults.headers.common["Authorization"] = BearerToken;
         return data;
     },
     async getCurrentStepObjectsOneByOne(objectList) {
-        const BearerToken = axios.defaults.headers.common["Authorization"];
-        delete axios.defaults.headers.common["Authorization"];
-
-        const url = `https://pennylane.pro/api/v1/get/index/`;
+        const url = `index/`;
         let data = false;
         let array = [];
         for (let index = 0; index < objectList.length; index++) {
-            delete axios.defaults.headers.common["Authorization"];
-
             const item = objectList[index];
             let objectUrl = url + `?id=${item.object_id}&type_id=${item.type_id}`;
-            await axios
+            await pennylaneAxios
                 .get(objectUrl)
                 .then((Response) => {
                     array.push(SuccessHandler.getData(Response));
@@ -45,25 +41,11 @@ export default {
         if (array.length) {
             data = array;
         }
+        return data;
+    },
 
-        axios.defaults.headers.common["Authorization"] = BearerToken;
-        return data;
-    },
-    async sendObjects(step_id, objects) {
-        const url = `timeline/add-objects/${step_id}`;
-        let data = false;
-        await axios
-            .post(url, objects)
-            .then((Response) => {
-                data = SuccessHandler.getData(Response);
-            })
-            .catch((e) => ErrorHandle.setError(e));
-        return data;
-    },
     async getAllObjects(page_num) {
-        const BearerToken = axios.defaults.headers.common["Authorization"];
-        delete axios.defaults.headers.common["Authorization"];
-        const url = `https://pennylane.pro/api/v1/get/list/`;
+        const url = `list/`;
         const formdata = new FormData();
         const params = {
             page_num: page_num,
@@ -71,7 +53,7 @@ export default {
         };
         formdata.append("request", JSON.stringify(params));
         let data = false;
-        await axios
+        await pennylaneAxios
             .post(url, formdata)
             .then((Response) => {
                 data = SuccessHandler.getData(Response);
@@ -87,8 +69,17 @@ export default {
             })
             .catch((e) => ErrorHandle.setError(e));
 
-        axios.defaults.headers.common["Authorization"] = BearerToken;
-
+        return data;
+    },
+    async sendObjects(step_id, objects) {
+        const url = `timeline/add-objects/${step_id}`;
+        let data = false;
+        await axios
+            .post(url, objects)
+            .then((Response) => {
+                data = SuccessHandler.getData(Response);
+            })
+            .catch((e) => ErrorHandle.setError(e));
         return data;
     }
 }
