c2d60141d9e54d96e6731b9fefe67ff3ad861a49
Абдурашидов Тимур <tim-a@realtor.ru>
2022-03-03 16:29:34 +0300

    add objects in deal link

--- src/views/Companies/Deals.vue
+++ src/views/Companies/Deals.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
-    <h1>deals</h1>
+  <div class="companies-deals">
+    <div class="row">
+      <div class="col-12">
+        <Objects>
+          <!-- <ObjectsSearch @search="search" class="mb-2" /> -->
+          <ObjectsList
+            :objects="allObjects"
+            :currentObjects="[]"
+            :selectedObjects="[]"
+            :disabled="true"
+            :loader="allObjectsLoader"
+            :viewMode="viewMode"
+            col="col-2"
+          />
+          <Pagination
+            :pagination="pagination"
+            @loadMore="loadMore"
+            class="text-center"
+          />
+        </Objects>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { MixinAllObject } from "@/components/companies/objects/mixins";
 export default {
+  mixins: [MixinAllObject],
   name: "CompaniesDeals",
+  methods: {
+    async getPreventStepObjects() {},
+  },
 };
 </script>
 
