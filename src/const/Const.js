5049a27ec65c0a2389305743c39d3ac0c13ee924
Абдурашидов Тимур <tim-a@realtor.ru>
2022-03-05 18:22:20 +0300

    change activityGroups order in const.js

--- src/const/Const.js
+++ src/const/Const.js
@@ -344,8 +344,8 @@ export const ActivityGroupList = new Map(
             { value: 2, label: "Строительство с привлечением" },
             { value: 3, label: "Строительство собственными силами" },
             { value: 4, label: "Услуги" },
-            { value: 6, label: "Управление активами/арендный бизнес" },
-            { value: 5, label: "Другое" },
+            { value: 5, label: "Управление активами/арендный бизнес" },
+            { value: 6, label: "Другое" },
         ]],
 
     ]
@@ -384,6 +384,7 @@ export const ActivityProfileList = new Map(
             { value: 28, label: "Полиграфия/бумага" },
             { value: 29, label: "ЖБИ/камень" },
             { value: 30, label: "Садовая/складская техника" },
+            { value: 31, label: "другое" },
         ]],
 
     ]
