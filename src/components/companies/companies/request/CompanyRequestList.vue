4ee3fdef5433bf1a9fa79285cb16479ef951107b
Абдурашидов Тимур <tim-a@realtor.ru>
2022-06-01 17:21:50 +0300

    add animation for delete and clone modals in request list

--- src/components/companies/companies/request/CompanyRequestList.vue
+++ src/components/companies/companies/request/CompanyRequestList.vue
@@ -1,67 +1,82 @@
 <template>
   <div class="company-request-list fuck">
-    <Modal
-      title="Удаление запроса "
-      @close="clickCloseModal"
-      v-if="deletedRequestItem"
-      class="action-modal"
+    <transition
+      mode="out-in"
+      enter-active-class="animate__animated animate__zoomIn for__modal absolute"
+      leave-active-class="animate__animated animate__zoomOut for__modal absolute"
     >
-      <div class="row no-gutters">
-        <div class="col-12 text-center">
-          <h4 class="text-dark">Вы уверены что хотите удалить запрос?</h4>
-          <CompanyRequestItem :request="deletedRequestItem" :reedOnly="true" />
+      <Modal
+        title="Удаление запроса "
+        @close="clickCloseModal"
+        v-if="deletedRequestItem"
+        class="action-modal"
+      >
+        <div class="row no-gutters">
+          <div class="col-12 text-center">
+            <h4 class="text-dark">Вы уверены что хотите удалить запрос?</h4>
+            <CompanyRequestItem
+              :request="deletedRequestItem"
+              :reedOnly="true"
+            />
+          </div>
+          <div class="col-12 mt-4 text-center">
+            <Loader class="center small" v-if="deleteLoader" />
+            <button
+              class="btn btn-danger"
+              :disabled="deleteLoader"
+              @click="deleteRequest(deletedRequestItem)"
+            >
+              Удалить
+            </button>
+            <button
+              class="btn btn-primary ml-1"
+              @click="clickCloseModal"
+              :disabled="deleteLoader"
+            >
+              Нет
+            </button>
+          </div>
         </div>
-        <div class="col-12 mt-4 text-center">
-          <Loader class="center small" v-if="deleteLoader" />
-          <button
-            class="btn btn-danger"
-            :disabled="deleteLoader"
-            @click="deleteRequest(deletedRequestItem)"
-          >
-            Удалить
-          </button>
-          <button
-            class="btn btn-primary ml-1"
-            @click="clickCloseModal"
-            :disabled="deleteLoader"
-          >
-            Нет
-          </button>
-        </div>
-      </div>
-    </Modal>
-    <Modal
-      title="Клонирование запроса "
-      @close="clickCloseModal"
-      v-if="clonedRequestItem"
-      class="action-modal"
+      </Modal>
+    </transition>
+    <transition
+      mode="out-in"
+      enter-active-class="animate__animated animate__zoomIn for__modal absolute"
+      leave-active-class="animate__animated animate__zoomOut for__modal absolute"
     >
-      <div class="row no-gutters">
-        <div class="col-12 text-center">
-          <h4 class="text-dark">
-            Вы уверены, что хотите клонировать этот запрос?
-          </h4>
-          <CompanyRequestItem :request="clonedRequestItem" :reedOnly="true" />
+      <Modal
+        title="Клонирование запроса "
+        @close="clickCloseModal"
+        v-if="clonedRequestItem"
+        class="action-modal"
+      >
+        <div class="row no-gutters">
+          <div class="col-12 text-center">
+            <h4 class="text-dark">
+              Вы уверены, что хотите клонировать этот запрос?
+            </h4>
+            <CompanyRequestItem :request="clonedRequestItem" :reedOnly="true" />
+          </div>
+          <div class="col-12 mt-4 text-center">
+            <Loader class="center small" v-if="cloneLoader" />
+            <button
+              class="btn btn-success"
+              :disabled="cloneLoader"
+              @click="cloneRequest(clonedRequestItem)"
+            >
+              Клонировать
+            </button>
+            <button
+              class="btn btn-primary ml-1"
+              @click="clickCloseModal"
+              :disabled="cloneLoader"
+            >
+              Нет
+            </button>
+          </div>
         </div>
-        <div class="col-12 mt-4 text-center">
-          <Loader class="center small" v-if="cloneLoader" />
-          <button
-            class="btn btn-success"
-            :disabled="cloneLoader"
-            @click="cloneRequest(clonedRequestItem)"
-          >
-            Клонировать
-          </button>
-          <button
-            class="btn btn-primary ml-1"
-            @click="clickCloseModal"
-            :disabled="cloneLoader"
-          >
-            Нет
-          </button>
-        </div>
-      </div>
-    </Modal>
+      </Modal>
+    </transition>
     <div class="row mb-2" v-if="requests.length">
       <div class="col-12 p-0">
         <div class="row no-gutters">
