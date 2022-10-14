16b0015b01eeac70085234409d6ff15efc84dffe
Абдурашидов Тимур <tim-a@realtor.ru>
2022-10-14 13:31:34 +0300

    fix bug in instection actions

--- src/components/companies/timeline/step-actions/InspectionActions.vue
+++ src/components/companies/timeline/step-actions/InspectionActions.vue
@@ -2,7 +2,7 @@
   <div class="step-action">
     <div class="row no-gutters inner scroller">
       <div class="col-12">
-        <div class="row px-2 pb-2" v-if="step.timelineStepObjects.length">
+        <div class="row px-2 pb-2">
           <div class="col-12">
             <div class="timeline-actions row">
               <Inspection
