6f6a2576054ae9c3f7794db2c209a33c4e6b4ad8
Павел <58857483+Haskiro@users.noreply.github.com>
2023-06-02 11:55:00 +0300

    property list item compnent update, about view data in a separate file

--- src/components/common/property-list/property-list-item/PropertyListItem.vue
+++ src/components/common/property-list/property-list-item/PropertyListItem.vue
@@ -1,19 +1,92 @@
 <template>
   <li class="PropertyListItem">
-    <div class="PropertyListItem-name">{{ name }}</div>
+    <div class="PropertyListItem-name">{{ name }}:</div>
     <p class="PropertyListItem-value" :class="{ 'not-filled': !value }">
       {{ propertyValue }}
       <span
+        v-if="unitType === unitTypes.SQUARE_METERS"
         class="PropertyListItem-value_small"
-        v-if="unit"
-        v-html="unitTypesList.get(unit)"
-      ></span>
+      >
+        м<sup>2</sup>
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.RUB_PER_MONTH"
+        class="PropertyListItem-value_small"
+      >
+        ₽/мес
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.METERS"
+        class="PropertyListItem-value_small"
+      >
+        м
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.KILOWATT"
+        class="PropertyListItem-value_small"
+      >
+        кВт
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.CUBE_METERS_PER_HOUR"
+        class="PropertyListItem-value_small"
+      >
+        м<sup>3</sup>/ч
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.BAR"
+        class="PropertyListItem-value_small"
+      >
+        бар
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.FLOORS"
+        class="PropertyListItem-value_small"
+      >
+        этаж(а)
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.SQUARE_METERS_PER_YEAR"
+        class="PropertyListItem-value_small"
+      >
+        м<sup>2</sup>/год
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.RUB"
+        class="PropertyListItem-value_small"
+      >
+        ₽
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.MONTH"
+        class="PropertyListItem-value_small"
+      >
+        мес
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.PERCENTS"
+        class="PropertyListItem-value_small"
+      >
+        %
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.YEAR"
+        class="PropertyListItem-value_small"
+      >
+        год
+      </span>
+      <span
+        v-else-if="unitType === unitTypes.PER_YEAR"
+        class="PropertyListItem-value_small"
+      >
+        в год
+      </span>
     </p>
   </li>
 </template>
 
 <script>
-import { UnitTypesList } from "@/const/Const";
+import { unitTypes } from "@/const/unitTypes";
 import { formatValue } from "@/utils";
 import "./styles.scss";
 
@@ -25,16 +98,17 @@ export default {
       default: null,
     },
     value: {
-      default: null,
+      type: String,
+      required: true,
     },
-    unit: {
+    unitType: {
       type: Number,
-      default: null,
+      required: true,
     },
   },
   data() {
     return {
-      unitTypesList: UnitTypesList,
+      unitTypes,
       formatValue,
     };
   },
