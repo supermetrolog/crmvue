47b61b629a885aeb405004b01cf4947b393704c4
Павел <58857483+Haskiro@users.noreply.github.com>
2023-06-13 16:16:44 +0300

    object data udpated, new styles, scroll if object owners overflow its container

--- src/components/object/object-holdings/object-holdings-owners/ObjectHoldingsOwnerList.vue
+++ src/components/object/object-holdings/object-holdings-owners/ObjectHoldingsOwnerList.vue
@@ -1,26 +1,30 @@
 <template>
-    <div class="ObjectHoldingsOwnerList">
-        <ObjectHoldingsOwnerItem v-for="(owner, idx) in owners" :key="idx" :owner="owner" :index="idx + 1" />
-    </div>
+  <div class="ObjectHoldingsOwnerList">
+    <ObjectHoldingsOwnerItem
+      v-for="(owner, idx) in owners"
+      :key="idx"
+      :owner="owner"
+      :index="idx + 1"
+    />
+  </div>
 </template>
 
 <script>
-import ObjectHoldingsOwnerItem from './object-holdings-owner-item/ObjectHoldingsOwnerItem.vue'
-import './styles.scss'
+import ObjectHoldingsOwnerItem from "./object-holdings-owner-item/ObjectHoldingsOwnerItem.vue";
+import "./styles.scss";
 export default {
-    name: 'ObjectHoldingsOwnerList',
-    components: { ObjectHoldingsOwnerItem },
-    props: {
-        owners: {
-            type: Array,
-            default: () => []
-        }
+  name: "ObjectHoldingsOwnerList",
+  components: { ObjectHoldingsOwnerItem },
+  props: {
+    owners: {
+      type: Array,
+      default: () => [],
     },
-    data() {
-        return {
-        }
-    },
-    computed: {},
-    methods: {}
-}
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
 </script>
--- src/components/object/object-holdings/object-holdings-parameters/ObjectHoldingsParameters.vue
+++ src/components/object/object-holdings/object-holdings-parameters/ObjectHoldingsParameters.vue
@@ -1,114 +1,123 @@
 <template>
   <div class="ObjectHoldingsParameters">
-    <div class="ObjectHoldingsParameters-main">
-      <p class="ObjectHoldingsParameters-main-area">
-        12652 м<sup>2</sup> (по этажам: 12662 м<sup>2</sup>)
-      </p>
-      <span class="ObjectHoldingsParameters-main-address"
-        >Московская область, Фрязино, Свидетельская 34</span
+    <div class="ObjectHoldingsParameters-wrapper">
+      <div class="ObjectHoldingsParameters-main">
+        <p class="ObjectHoldingsParameters-main-area">
+          <with-unit-type
+            :value="formattedArea"
+            :unitType="unitTypes.SQUARE_METERS"
+          />
+          (по этажам:
+          <with-unit-type
+            :value="formattedFloorArea"
+            :unitType="unitTypes.SQUARE_METERS"
+          />
+          )
+        </p>
+        <p class="ObjectHoldingsParameters-main-address">{{ fullAddress }}</p>
+      </div>
+      <div class="ObjectHoldingsParameters-types" v-if="holdingTypes">
+        <template v-if="holdingTypes.length > 0">
+          <strong
+            class="object-type-box"
+            v-for="objectType of holdingTypes"
+            :key="objectType.id"
+            :title="getObjectTypeName(objectType.object_type)"
+          >
+            <i :class="getObjectTypeIcon(objectType.object_type)"></i>
+          </strong>
+        </template>
+        <div v-if="holdingTypes.length == 0 && holdingTypesGeneral.length == 0">
+          <p>нет данных</p>
+        </div>
+      </div>
+      <div class="ObjectHoldingsParameters-provision">
+        <parameters
+          :height="parameters.characteristics.height"
+          :floorType="parameters.characteristics.floorType"
+          :gatesNumber="parameters.characteristics.gatesNumber"
+          :electricity="parameters.communications.electricity"
+          :heating="parameters.communications.heating"
+          :sewage="parameters.communications.sewage"
+          :gasForProduction="parameters.communications.gasForProduction"
+          :liftingDevices="parameters.liftingDevices.lifts"
+          :shelving="parameters.facilities.shelving"
+        />
+      </div>
+      <!-- <div class="ObjectHoldingsParameters-equipment">
+        <p>Краны</p>
+        <div>
+          <span>Тельфер/лебедка (неподвижн)</span>
+        </div>
+      </div>
+      <div class="ObjectHoldingsParameters-equipment">
+        <p>Подъемники</p>
+        <div>
+          <span>Грузовой лифт</span>
+        </div>
+      </div> -->
+    </div>
+    <div class="ObjectHoldingsParameters-floors" v-if="floors.length > 0">
+      <div
+        class="ObjectHoldingsParameters-floor"
+        :class="{ danger: floor.danger }"
+        v-for="floor in floors"
+        :key="floor.number"
       >
-    </div>
-    <div class="ObjectHoldingsParameters-types" v-if="holdingTypes">
-      <template v-if="holdingTypes.length > 0">
-        <strong
-          class="object-type-box"
-          v-for="objectType of holdingTypes"
-          :key="objectType.id"
-          :title="getObjectTypeName(objectType.object_type)"
-        >
-          <i :class="getObjectTypeIcon(objectType.object_type)"></i>
-        </strong>
-      </template>
-      <!-- <div v-if="holdingTypesGeneral.length > 0">
-                <strong>{{
-                    getObjectTypesGeneral(holdingTypesGeneral)
-                }}</strong>
-            </div> -->
-      <div v-if="holdingTypes.length == 0 && holdingTypesGeneral.length == 0">
-        <p>нет данных</p>
+        <span>{{ floor.number }} этаж</span>
+        <i
+          class="fas fa-exclamation-circle text-danger"
+          v-if="floor.danger"
+          title="Внимание"
+        ></i>
       </div>
-    </div>
-    <div class="ObjectHoldingsParameters-provision">
-      <div class="ObjectHoldingsParameters-provision-item">
-        <i class="fas fa-arrow-up"></i>
-        <span>1-15 метров</span>
-      </div>
-      <div class="ObjectHoldingsParameters-provision-item">
-        <i class="fas fa-dungeon"></i>
-        <span>8 ворот</span>
-      </div>
-      <div class="ObjectHoldingsParameters-provision-item">
-        <i class="fas fa-arrow-down"></i>
-        <span>Антипыль</span>
-      </div>
-      <div class="ObjectHoldingsParameters-provision-item">
-        <i class="fas fa-temperature-low"></i>
-        <span>Неотапливаемый</span>
-      </div>
-      <div class="ObjectHoldingsParameters-provision-item">
-        <i class="fas fa-temperature-high"></i>
-        <span>Отопление</span>
-      </div>
-      <div class="ObjectHoldingsParameters-provision-item">
-        <i class="fas fa-fire"></i>
-        <span>Газ в цеху</span>
-      </div>
-      <div class="ObjectHoldingsParameters-provision-item">
-        <i class="fas fa-bolt"></i>
-        <span>1500 кВт</span>
-      </div>
-    </div>
-    <div class="ObjectHoldingsParameters-equipment">
-      <p>Краны</p>
-      <div>
-        <span>Тельфер/лебедка (неподвижн)</span>
-      </div>
-    </div>
-    <div class="ObjectHoldingsParameters-equipment">
-      <p>Подъемники</p>
-      <div>
-        <span>Грузовой лифт</span>
-      </div>
-    </div>
-  </div>
-  <div class="ObjectHoldingsParameters-floors" v-if="floors.length > 0">
-    <div
-      class="ObjectHoldingsParameters-floor"
-      :class="{ danger: floor.danger }"
-      v-for="floor in floors"
-      :key="floor.number"
-    >
-      <span>{{ floor.number }} этаж</span>
-      <i
-        class="fas fa-exclamation-circle text-danger"
-        v-if="floor.danger"
-        title="Внимание"
-      ></i>
     </div>
   </div>
 </template>
 
 <script>
-import {
-  ObjectTypeList,
-  // ObjectTypesGeneralList
-} from "@/const/Const.js";
+import { ObjectTypeList } from "@/const/Const.js";
 import "./styles.scss";
+import Parameters from "../../ui/parameters/Parameters.vue";
+import WithUnitType from "@/components/common/with-unit-type/WithUnitType.vue";
+import { unitTypes } from "@/const/unitTypes";
 export default {
   name: "ObjectHoldingsParameters",
-  components: {},
+  components: {
+    Parameters,
+    WithUnitType,
+  },
   props: {
     holdingTypes: {
       type: Array,
       default: () => [],
+      required: true,
     },
     holdingTypesGeneral: {
       type: Array,
       default: () => [],
+      required: true,
     },
     floors: {
       type: Array,
       default: () => [],
+      required: true,
+    },
+    parameters: {
+      type: Object,
+      required: true,
+    },
+    area: {
+      type: Object,
+      required: true,
+    },
+    floorArea: {
+      type: Object,
+      required: true,
+    },
+    address: {
+      type: Object,
+      required: true,
     },
   },
   data() {
@@ -116,6 +125,7 @@ export default {
       objectTypeListWareHouse: ObjectTypeList.get("warehouse"),
       objectTypeListProduction: ObjectTypeList.get("production"),
       objectTypeListPlot: ObjectTypeList.get("plot"),
+      unitTypes,
       // objectTypesGeneralList: ObjectTypesGeneralList.get("param"),
     };
   },
@@ -157,6 +167,31 @@ export default {
     //         )
     //         .join(", ");
     // },
+  },
+  computed: {
+    formattedArea() {
+      return this.$formatter.numberOrRange(
+        this.area.valueMin,
+        this.area.valueMax
+      );
+    },
+    formattedFloorArea() {
+      return this.$formatter.numberOrRange(
+        this.floorArea.valueMin,
+        this.floorArea.valueMax
+      );
+    },
+    fullAddress() {
+      return (
+        this.address.district +
+        ", " +
+        this.address.city +
+        ", " +
+        this.address.street +
+        " " +
+        this.address.houseNumber
+      );
+    },
   },
 };
 </script>
