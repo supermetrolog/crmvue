e6c9ee2883394c19e7d5e83f1a059d38494a6556
Павел <58857483+Haskiro@users.noreply.github.com>
2023-06-14 14:26:59 +0300

    table section labels  with styles added

--- src/components/object/trade-offer-details-table/TradeOfferDetailsTable.vue
+++ src/components/object/trade-offer-details-table/TradeOfferDetailsTable.vue
@@ -1,19 +1,54 @@
 <template>
-  <div class="trade-offer-details-table">Details</div>
+  <div class="trade-offer-details-table">
+    <ul class="trade-offer-details-table__column">
+      <li
+        class="trade-offer-details-table__head trade-offer-details-table__head_color_grey"
+      >
+        Этажи
+      </li>
+      <li
+        class="trade-offer-details-table__section trade-offer-details-table__section_label"
+      >
+        <p class="trade-offer-details-table__text">№ блока:</p>
+      </li>
+      <li
+        v-for="parameter in Object.keys(tradeOfferCharacteristics)"
+        :key="parameter"
+        class="trade-offer-details-table__section trade-offer-details-table__section_label"
+      >
+        <p
+          class="trade-offer-details-table__text trade-offer-details-table__text_weight_bold"
+        >
+          {{ parameterTypes[parameter] }}
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 import { ITradeOfferBlock } from "../trade-offer-tabs/tradeOfferTabs.interface";
 import { PropType, defineComponent } from "vue";
+import { tradeOfferCharacteristics } from "@/const/tradeOfferCharacteristics";
+import { parameterTypes } from "@/const/parameterTypes";
+
+const tableHeadColors = ["green", "blue", "cyan", "orange", "red"];
 
 export default defineComponent({
   name: "TradeOfferDetailsTable",
   components: {},
   props: {
     blocks: {
-      type: Object as PropType<ITradeOfferBlock>,
+      type: Object as PropType<ITradeOfferBlock[]>,
       requrired: true,
     },
+  },
+  data() {
+    return {
+      tableHeadColors,
+      tradeOfferCharacteristics,
+      parameterTypes,
+    };
   },
 });
 </script>
---
+++ src/const/parameterTypes.ts
@@ -0,0 +1,7 @@
+export const parameterTypes: Record<string, string> = {
+  characteristics: "Характеристики",
+  facilities: "Оборудование",
+  communications: "Коммуникации",
+  securitySystems: "Системы безопасности",
+  liftingDevices: "Подъемные устройства",
+};
