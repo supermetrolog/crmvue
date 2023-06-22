0e0bc5f059344c386cb8a53f1c89c53fe650ca97
Павел <58857483+Haskiro@users.noreply.github.com>
2023-06-22 17:55:03 +0300

    property list item was updated with slot, first property lists were updated with real data from api

--- src/components/common/property-list/property-list-item/PropertyListItem.vue
+++ src/components/common/property-list/property-list-item/PropertyListItem.vue
@@ -1,85 +1,33 @@
 <template>
   <li class="PropertyListItem">
-    <div class="PropertyListItem-name">{{ name }}:</div>
-    <with-unit-type
-      class="PropertyListItem-value"
-      :class="{
-        'not-filled': !valueExists,
-      }"
-      :unitType="displayUnit"
-      :value="propertyValue"
-    />
+    <p class="PropertyListItem-name">{{ name }}:</p>
+    <div class="PropertyListItem-value">
+      <slot>
+        не заполнено
+      </slot>
+    </div>
   </li>
 </template>
 
 <script lang="ts">
 import { unitTypes } from "@/const/unitTypes";
 import { defineComponent } from "vue";
-import WithUnitType from "../../with-unit-type/WithUnitType.vue";
 import "./styles.scss";
 
 export default defineComponent({
   name: "PropertyListItem",
   components: {
-    WithUnitType,
   },
   props: {
     name: {
       type: String,
-      default: null,
-    },
-    value: {
-      type: [String, Number, Boolean],
-    },
-    valueMin: {
-      type: Number,
-    },
-    valueMax: {
-      type: Number,
-    },
-    unitType: {
-      type: [Number],
-      // required: true,
+      default: "Дефолтное значение",
     },
   },
   data() {
     return {
       unitTypes,
     };
-  },
-
-  methods: {
-    isNullish(value: any): boolean {
-      return value === null || value === undefined;
-    },
-  },
-  computed: {
-    propertyValue() {
-      if (!this.isNullish(this.value) && typeof this.value === "string")
-        return this.value;
-      if (!this.isNullish(this.value) && typeof this.value === "boolean")
-        return this.value ? "есть" : "нет";
-      if (!this.isNullish(this.value) && typeof this.value === "number")
-        return this.$formatter.numberOrRangeNew(this.value, this.value);
-      if (!this.isNullish(this.valueMin) && !this.isNullish(this.valueMax)) {
-        return this.$formatter.numberOrRangeNew(this.valueMin, this.valueMax);
-      }
-      return "не заполнено";
-    },
-    displayUnit() {
-      return this.valueExists &&
-        this.propertyValue !== "есть" &&
-        this.propertyValue !== "нет"
-        ? this.unitType
-        : undefined;
-    },
-    valueExists() {
-      return !(
-        this.isNullish(this.value) &&
-        this.isNullish(this.valueMin) &&
-        this.isNullish(this.valueMax)
-      );
-    },
   },
 });
 </script>
--- src/components/object/complex-about/ComplexAbout.vue
+++ src/components/object/complex-about/ComplexAbout.vue
@@ -1,55 +1,144 @@
 <template>
   <div class="ComplexAbout" id="ComplexAbout">
     <Tabs :options="{ useUrlFragment: false }">
-      <Tab name="О комплексе">
+      <Tab name="О комплексе" v-if="complex">
         <div class="ComplexAbout-content">
           <template v-if="dataAvailable">
-<!--            <PropertyList :title="'Площади'">-->
-<!--              <PropertyListItem-->
-<!--                v-for="(prop, idx) in area"-->
-<!--                :key="prop.name + idx"-->
-<!--                :name="prop.name"-->
-<!--                :value="prop.value"-->
-<!--                :valueMin="prop.valueMin"-->
-<!--                :valueMax="prop.valueMax"-->
-<!--                :unitType="prop.unitType"-->
-<!--              />-->
-<!--            </PropertyList>-->
-<!--            <PropertyList :title="'Коммуникации'">-->
-<!--              <PropertyListItem-->
-<!--                v-for="(prop, idx) in communications"-->
-<!--                :key="prop.name + idx"-->
-<!--                :name="prop.name"-->
-<!--                :value="prop.value"-->
-<!--                :unitType="prop.unitType"-->
-<!--              />-->
-<!--            </PropertyList>-->
+            <PropertyList title="Площади">
+              <PropertyListItem name="S - участка общая">
+                <with-unit-type class="ComplexAbout-property" v-if="complex.areaFieldFull !== null"
+                                :unit-type="unitTypes.SQUARE_METERS"
+                                :value="$formatter.number(complex.areaFieldFull)"/>
+              </PropertyListItem>
+              <PropertyListItem name="S - строений в комплексе">
+                <with-unit-type class="ComplexAbout-property" v-if="complex.areaBuilding !== null"
+                                :unit-type="unitTypes.SQUARE_METERS"
+                                :value="$formatter.number(complex.areaBuilding)"/>
+              </PropertyListItem>
+              <PropertyListItem name="S - этажей общая">
+                <with-unit-type class="ComplexAbout-property" v-if="complex.areaFloorFull !== null"
+                                :unit-type="unitTypes.SQUARE_METERS"
+                                :value="$formatter.number(complex.areaFloorFull)"/>
+              </PropertyListItem>
+              <PropertyListItem name="S - оффисов общая">
+                <with-unit-type class="ComplexAbout-property" v-if="complex.areaOfficeFull !== null"
+                                :unit-type="unitTypes.SQUARE_METERS"
+                                :value="$formatter.number(complex.areaOfficeFull)"/>
+              </PropertyListItem>
+              <PropertyListItem name="S - техническая общая">
+                <with-unit-type class="ComplexAbout-property" v-if="complex.areaTechFull !== null"
+                                :unit-type="unitTypes.SQUARE_METERS"
+                                :value="$formatter.number(complex.areaTechFull)"/>
+              </PropertyListItem>
+              <PropertyListItem name="Управляющая компания">
+                <p v-if="complex.managmentCompany !== null" class="ComplexAbout-property">
+                  {{ managmentCompany }}
+                </p>
+              </PropertyListItem>
+            </PropertyList>
+            <PropertyList title="Коммуникации">
+              <PropertyListItem name="Электричество">
+                <template v-if="complex.power !== null">
+                  <with-unit-type class="ComplexAbout-property" v-if="complex.power"
+                                  :unit-type="unitTypes.KILOWATT"
+                                  :value="$formatter.number(complex.powerValue)"/>
+                  <p v-else class="ComplexAbout-property">
+                    нет
+                  </p>
+                </template>
+              </PropertyListItem>
+              <PropertyListItem name="Отопление центральное">
+                <p v-if="complex.heating !== null" class="ComplexAbout-property">
+                  {{ isExists(complex.heating) }}
+                </p>
+              </PropertyListItem>
+              <PropertyListItem name="Отопление автономное">
+                <template v-if="complex.heatingAutonomous !== null">
+                  <p v-if="complex.heatingAutonomous" class="ComplexAbout-property">
+                    есть, <span class="ComplexAbout-property-grey">{{complex.heatingAutonomousType}}</span>
+                  </p>
+                  <p v-else class="ComplexAbout-property">
+                    нет
+                  </p>
+                </template>
+              </PropertyListItem>
+              <PropertyListItem name="Водоснабжение">
+                <p v-if="complex.water !== null" class="ComplexAbout-property">
+                  {{ isExists(complex.water) }}
+                </p>
+              </PropertyListItem>
+              <PropertyListItem name="Канализация">
+                <p v-if="complex.sewage !== null" class="ComplexAbout-property">
+                  {{ isExists(complex.sewage) }}
+                </p>
+              </PropertyListItem>
+              <PropertyListItem name="Ливневая канал.">
+                <p v-if="complex.sewageRain !== null" class="ComplexAbout-property">
+                  {{ isExists(complex.sewageRain) }}
+                </p>
+              </PropertyListItem>
+              <PropertyListItem name="Газ">
+                <template v-if="complex.gas !== null">
+                  <with-unit-type class="ComplexAbout-property" v-if="complex.gas"
+                                  :unit-type="unitTypes.CUBE_METERS_PER_HOUR"
+                                  :value="$formatter.number(complex.gasValue)"/>
+                  <p v-else class="ComplexAbout-property">
+                    нет
+                  </p>
+                </template>
+              </PropertyListItem>
+              <PropertyListItem name="Пар">
+                <template v-if="complex.steam !== null">
+                  <with-unit-type class="ComplexAbout-property" v-if="complex.steam"
+                                  :unit-type="unitTypes.CUBE_METERS_PER_HOUR"
+                                  :value="$formatter.number(complex.steamValue)"/>
+                  <p v-else class="ComplexAbout-property">
+                    нет
+                  </p>
+                </template>
+              </PropertyListItem>
+              <PropertyListItem name="Телефония">
+                <p v-if="complex.phoneLine !== null" class="ComplexAbout-property">
+                  {{ isExists(complex.phoneLine) }}
+                </p>
+              </PropertyListItem>
+              <PropertyListItem name="Интернет">
+                <template v-if="complex.internet !== null">
+                  <p v-if="complex.internet" class="ComplexAbout-property">
+                    есть, <span class="ComplexAbout-property-grey">{{complex.internetType}}</span>
+                  </p>
+                  <p v-else class="ComplexAbout-property">
+                    нет
+                  </p>
+                </template>
+              </PropertyListItem>
+            </PropertyList>
             <PropertyList :title="'Безопасность'">
               <PropertyListItem
-                v-for="(prop, idx) in safety"
-                :key="prop.name + idx"
-                :name="prop.name"
-                :value="prop.value"
-                :unitType="prop.unitType"
+                  v-for="(prop, idx) in safety"
+                  :key="prop.name + idx"
+                  :name="prop.name"
+                  :value="prop.value"
+                  :unitType="prop.unitType"
               />
             </PropertyList>
             <PropertyList :title="['Ж/Д на территории', 'Инфраструктура']">
               <template #0>
                 <PropertyListItem
-                  v-for="(prop, idx) in railway"
-                  :key="prop.name + idx"
-                  :name="prop.name"
-                  :value="prop.value"
-                  :unitType="prop.unitType"
+                    v-for="(prop, idx) in railway"
+                    :key="prop.name + idx"
+                    :name="prop.name"
+                    :value="prop.value"
+                    :unitType="prop.unitType"
                 />
               </template>
               <template #1>
                 <PropertyListItem
-                  v-for="(prop, idx) in infrastructure"
-                  :key="prop.name + idx"
-                  :name="prop.name"
-                  :value="prop.value"
-                  :unitType="prop.unitType"
+                    v-for="(prop, idx) in infrastructure"
+                    :key="prop.name + idx"
+                    :name="prop.name"
+                    :value="prop.value"
+                    :unitType="prop.unitType"
                 />
               </template>
             </PropertyList>
@@ -70,17 +159,21 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import PropertyListItem from "../../common/property-list/property-list-item/PropertyListItem.vue";
 import PropertyList from "../../common/property-list/PropertyList.vue";
 import "./styles.scss";
+import WithUnitType from "@/components/common/with-unit-type/WithUnitType.vue";
+import {unitTypes} from "@/const/unitTypes";
+import {defineComponent, PropType} from "vue";
+import IComplex from "@/interfaces/complex.interface";
 
-export default {
+export default defineComponent({
   name: "ComplexAbout",
-  components: { PropertyList, PropertyListItem },
+  components: {WithUnitType, PropertyList, PropertyListItem},
   props: {
     complex: {
-      type: Object
+      type: Object as PropType<IComplex>
     },
     safety: {
       type: Array,
@@ -93,20 +186,29 @@ export default {
     },
   },
   data() {
-    return {};
+    return {
+      unitTypes
+    };
   },
   computed: {
     dataAvailable() {
       return (
-        // this.area &&
-        // this.communications &&
-        this.safety &&
-        this.railway &&
-        this.infrastructure
+          // this.area &&
+          // this.communications &&
+          this.safety &&
+          this.railway &&
+          this.infrastructure
       );
     },
+    managmentCompany() {
+      return this.complex?.managmentCompany ? this.complex.managmentCompanyValue : "нет"
+    }
   },
-  methods: {},
+  methods: {
+    isExists(value: boolean): string {
+      return value ? "есть" : "нет";
+    }
+  },
   mounted() {
     if (this.isMobile) {
       return;
@@ -119,5 +221,5 @@ export default {
       });
     }
   },
-};
+});
 </script>
