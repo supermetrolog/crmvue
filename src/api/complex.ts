9c7816e6c2bd274d0afdb5858f15ab9614cfd99c
Павел <58857483+Haskiro@users.noreply.github.com>
2023-07-06 12:09:55 +0300

    create crane and elevator forms validation

--- src/api/complex.ts
+++ src/api/complex.ts
@@ -3,22 +3,22 @@ import ErrorHandle from "./errors";
 import SuccessHandler from "./success";
 
 export default {
-	async fetch(id: string, params?: Record<string, string>) {
-		const query = new URLSearchParams(params).toString();
-		const url = query ? `complex/${id}?${query}` : `comples/${id}`;
-		try {
-			const response = await axios.get(url);
-			return {
-				data: SuccessHandler.getData(response),
-				pagination: SuccessHandler.getPaginationData(response),
-			};
-		} catch (e) {
-			ErrorHandle.setError(e);
-		}
-	},
-	async getComplexWithObjects(complexId: string) {
-		return await this.fetch(complexId, {
-			expand: `objects.company.consultant.userProfile,
+    async fetch(id: string, params?: Record<string, string>) {
+        const query = new URLSearchParams(params).toString();
+        const url = query ? `complex/${id}?${query}` : `comples/${id}`;
+        try {
+            const response = await axios.get(url);
+            return {
+                data: SuccessHandler.getData(response),
+                pagination: SuccessHandler.getPaginationData(response),
+            };
+        } catch (e) {
+            ErrorHandle.setError(e);
+        }
+    },
+    async getComplexWithObjects(complexId: string) {
+        return await this.fetch(complexId, {
+            expand: `objects.company.consultant.userProfile,
           objects.objectClassRecord,
           objects.firefightingType,
           objects.location.regionRecord,
@@ -37,6 +37,6 @@ export default {
           guardTypes,
           internetTypes,
           waterTypes`
-		});
-	},
+        });
+    },
 };
--- src/const/liftingDevices.ts
+++ src/const/liftingDevices.ts
@@ -1,47 +1,47 @@
 export const craneTypes: Record<number, string> = {
-	1: "Кран-балка",
-	2: "Мостовой",
-	3: "Козловой",
-	4: "Тельфер/Лебедка(неподвиж)",
+    1: "Кран-балка",
+    2: "Мостовой",
+    3: "Козловой",
+    4: "Тельфер/Лебедка(неподвиж)",
 }
 
 export const elevatorTypes: Record<number, string> = {
-	1: "Подъемник",
-	2: "Грузовой лифт",
-	3: "Гидроплатформа",
+    1: "Подъемник",
+    2: "Грузовой лифт",
+    3: "Гидроплатформа",
 }
 
 export const liftingDeviceLocationTypes: Record<number, string> = {
-	1: "Внутри",
-	2: "Снаружи",
+    1: "Внутри",
+    2: "Снаружи",
 }
 
 export const craneBeamTypes: Record<number, string> = {
-	1: "Опорная",
-	2: "Подвесная",
-	3: "С передвижными опорами",
+    1: "Опорная",
+    2: "Подвесная",
+    3: "С передвижными опорами",
 }
 
 export const craneBeamsAmountTypes: Record<number, string> = {
-	1: "Одна",
-	2: "Две",
-	3: "Три",
-	4: "Четыре",
-	5: "Пять",
+    1: "Одна",
+    2: "Две",
+    3: "Три",
+    4: "Четыре",
+    5: "Пять",
 }
 
 export const craneHoistingTypes: Record<number, string> = {
-	1: "Подвесной",
-	2: "На тележке(опирается сверху)",
+    1: "Подвесной",
+    2: "На тележке(опирается сверху)",
 }
 
 export const liftingDeviceControlsTypes: Record<number, string> = {
-	1: "С пола",
-	2: "Из кабины",
+    1: "С пола",
+    2: "Из кабины",
 }
 
 export const liftingDeviceConditionTypes: Record<number, string> = {
-	1: "Рабочий",
-	2: "Требует ремонта",
-	3: "Требуется техобслуживание",
+    1: "Рабочий",
+    2: "Требует ремонта",
+    3: "Требуется техобслуживание",
 }
