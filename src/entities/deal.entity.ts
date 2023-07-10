ed6d38d5e18c9dfdce3f77f94715f6bf7493a497
Haskiro <kondratev.pacha@mail.ru>
2023-07-10 17:40:19 +0300

    deal entity new fields

--- src/entities/deal.entity.ts
+++ src/entities/deal.entity.ts
@@ -1,6 +1,6 @@
 import BaseEntity from "@/entities/base.entity";
 import {IDeal} from "@/interfaces/deal.interface";
-import {DealStatusList} from "@/const/Const";
+import {DealStatusList, TaxFormList} from "@/const/Const";
 import {CompanyEntity} from "@/entities/company.entity";
 import {ICompany} from "@/interfaces/company.interface";
 
@@ -22,9 +22,70 @@ export class DealEntity extends BaseEntity implements IDeal {
         return this._status_id ? DealStatusList[this._status_id] : null;
     }
 
-    public id: bigint | null = null;
 
+    get holidays(): boolean | null {
+        return this.processBooleanField(this._holidays);
+    }
+
+    get deposit(): boolean | null {
+        return this.processBooleanField(this._deposit);
+    }
+
+    get pledge(): boolean | null {
+        return this.processBooleanField(this._pledge);
+    }
+
+    get is_exclusive(): boolean | null {
+        return this.processBooleanField(this._is_exclusive);
+    }
+
+    get contract_is_signed(): boolean | null {
+        return this.processBooleanField(this._contract_is_signed);
+    }
+
+    get commission_owner(): boolean | null {
+        return this._commission_owner ? !!(this._commission_owner === 1) : null;
+    }
+
+    get commission_owner_type(): string | null {
+        // return this._commission_owner_type;
+        return "Деньгами(гарантированно)";
+    }
+
+    get commission_client(): boolean | null {
+        return this._commission_client ? !!(this._commission_client === 1) : null;
+    }
+
+    get commission_agent(): boolean | null {
+        return this._commission_agent ? !!(this._commission_agent === 1) : null;
+    }
+
+    get tax_form(): string | null {
+        const taxStr = TaxFormList.find(el => el.value === this._tax_form)
+        return taxStr ? taxStr.label : null;
+    }
+
+    public id: bigint | null = null;
+    public price_opex_value: number | null = null;
+    public price_public_services: number | null = null;
+    public holidays_value_min: number | null = null;
+    public holidays_value_max: number | null = null;
+    public deposit_value: number | null = null;
+    public commission_owner_value: number | null = null;
+    public commission_client_value: number | null = null;
+    public commission_agent_value: number | null = null;
+
+    private _holidays: number | null = null;
+    private _deposit: number | null = null;
+    private _pledge: number | null = null;
+    private _is_exclusive: number | null = null;
+    private _contract_is_signed: number | null = null;
+    private _commission_owner: number | null = null;
+    private _commission_owner_type: number | null = null;
+    private _tax_form: number | null = null;
+    private _commission_client: number | null = null;
+    private _commission_agent: number | null = null;
     private _company: Record<string, any> | null = null;
-    private _dealTypeRecord: {title: string} | null = null;
+    private _dealTypeRecord: { title: string } | null = null;
     private _status_id: number | null = null;
 }
--- src/interfaces/deal.interface.ts
+++ src/interfaces/deal.interface.ts
@@ -5,4 +5,23 @@ export interface IDeal {
     company: ICompany | null;
     deal_type: string | null;
     status: string | null;
+    price_opex_value: number | null;
+    price_public_services: number | null;
+    holidays: boolean | null;
+    holidays_value_min: number | null;
+    holidays_value_max: number | null;
+    deposit: boolean | null;
+    deposit_value: number | null;
+    pledge: boolean | null;
+    is_exclusive: boolean | null;
+    contract_is_signed: boolean | null;
+    commission_owner: boolean | null;
+    commission_owner_value: number | null;
+    commission_owner_type: string | null;
+    commission_client: boolean | null;
+    commission_client_value: number | null;
+    commission_agent: boolean | null;
+    commission_agent_value: number | null;
+    tax_form: string | null;
+
 }
