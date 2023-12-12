b79eafb354f70ba7305a57c82c238945d9b14fe9
nknwns <andrejshaev@gmail.com>
2023-12-12 15:22:47 +0300

    feat: create pagination and related components

---
+++ src/components/Pagination/PaginationPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <button
+        class="pagination-classic__button"
+        :class="{'pagination-classic__button--navigate': navigate, active: active}"
+        @click.prevent="$emit('click')"
+        :disabled="disabled"
+    >
+        <slot/>
+    </button>
+</template>
+<script>
+
+export default {
+    name: "PaginationPage",
+    emits: ['click'],
+    props: {
+        navigate: {
+            type: Boolean,
+            default: false
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        active: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
