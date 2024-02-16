8810245c466d2410105e2c08c266dbaf8dae451c
nknwns <andrejshaev@gmail.com>
2024-02-16 20:32:17 +0300

    feat: add unit types

---
+++ src/components/common/UnitTypes/UnitTypeRubPerSquareMeters.vue
@@ -0,0 +1,9 @@
+<template>
+    <span class="unit-type">₽ за</span>
+    <span class="unit-type unit-type--size_small">м<sup>2</sup></span>
+</template>
+<script>
+export default {
+    name: 'UnitTypeRubPerSquareMeters'
+};
+</script>
--- src/components/common/UnitTypes/UnitTypeRubPerSquareMetersPerMonth.vue
+++ src/components/common/UnitTypes/UnitTypeRubPerSquareMetersPerMonth.vue
@@ -1,6 +1,6 @@
 <template>
-    <span class="unit-type"> ₽ </span>
-    <span class="unit-type unit-type--size_small"> м<sup>2</sup>/мес </span>
+    <span class="unit-type">₽ за</span>
+    <span class="unit-type unit-type--size_small">м<sup>2</sup>/мес</span>
 </template>
 <script>
 export default {
