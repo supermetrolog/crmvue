c97376c24d55e1c7fef59eeb88752c568106b17c
nknwns <andrejshaev@gmail.com>
2024-02-16 21:09:50 +0300

    feat: add info about adv and services in tradeOffer

---
+++ src/components/Complex/Offer/ComplexOfferServices.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="trade-offer-services">
+        <div
+            v-for="(category, index) in services"
+            :key="index"
+            class="trade-offer-services__category"
+        >
+            <p class="trade-offer-services__category">{{ category.name }}</p>
+            <ul class="trade-offer-services__list">
+                <li
+                    v-for="(service, key) in category.properties"
+                    :key="`${index}-${key}`"
+                    class="trade-offer-services__item"
+                >
+                    <i v-if="service.icon" :class="service.icon" />
+                    <span>{{ service.name }}</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ComplexOfferServices',
+    props: {
+        services: {
+            type: Array,
+            default: () => []
+        }
+    }
+};
+</script>
