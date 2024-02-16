9ee5df82fff9481f984e589e594c86ba20541a0f
nknwns <andrejshaev@gmail.com>
2024-02-16 21:07:58 +0300

    feat: add ComplexActions, add info about parts in tradeOffer

---
+++ src/components/Complex/Offer/ComplexOfferParts.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="trade-offer-parts">
+        <div v-for="floor in filteredFloors" :key="floor.id" class="trade-offer-floor">
+            <span class="trade-offer-floor__title">{{ floor.number.title }}</span>
+            <ul class="trade-offer-floor__list">
+                <li v-for="part in floor.parts" :key="part.id" class="trade-offer-floor__item">
+                    <ComplexOfferPartsItem :class="{ disabled: !part.active }" :part="part" />
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+import ComplexOfferPartsItem from '@/components/Complex/Offer/ComplexOfferPartsItem.vue';
+
+export default {
+    name: 'ComplexOfferParts',
+    components: { ComplexOfferPartsItem },
+    props: {
+        floors: {
+            type: Array,
+            default: () => []
+        }
+    },
+    computed: {
+        filteredFloors() {
+            return this.floors.filter(floor => floor.parts.length);
+        }
+    }
+};
+</script>
