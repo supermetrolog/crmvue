fb028c6a7a9c95503dbbeb6f541cad55925aba86
nknwns <andrejshaev@gmail.com>
2024-02-16 20:52:50 +0300

    chore: improve Crane and Elevator views in ComplexObject, add icons

--- src/components/Complex/Holding/ComplexHoldingParametersCrane.vue
+++ src/components/Complex/Holding/ComplexHoldingParametersCrane.vue
@@ -1,60 +1,87 @@
 <template>
     <li class="object-equipment">
-        <span v-if="crane.crane_type">{{ crane.type.title }}</span>
-        <span v-if="crane.crane_capacity"> / {{ crane.crane_capacity }} тонн</span>
-        <span v-if="crane.crane_location"> / {{ crane.location.title }} </span>
-        <span v-if="crane.crane_hook_height"> / до крюка {{ crane.crane_hook_height }} м </span>
-        <span v-if="crane.crane_condition"> / {{ crane.crane_condition }} </span>
+        <span v-if="crane.state && crane.state.id > 1" class="object-equipment__info"></span>
+        <Tooltip>
+            <template #trigger>
+                <IconCrane class="object-equipment__icon" />
+                <p v-if="crane.crane_type" class="object-equipment__header">
+                    {{ crane.type.title }}
+                </p>
+                <p v-if="crane.crane_capacity" class="object-equipment__header">
+                    {{ crane.crane_capacity }} тонн
+                </p>
+            </template>
+            <template #content>
+                <span v-if="status" class="object-equipment__status">{{ status }}</span>
+                <p v-if="crane.crane_type" class="object-equipment__title">
+                    {{ crane.type.title }}
+                </p>
+                <p
+                    v-for="(property, key) in properties"
+                    :key="key"
+                    class="object-equipment__element"
+                >
+                    <span class="object-equipment__category">{{ property.label }}:</span>
+                    <with-unit-type v-if="property.unitType" :unit-type="property.unitType">
+                        {{ property.value }}
+                    </with-unit-type>
+                    <span v-else>{{ property.value }}</span>
+                </p>
+            </template>
+        </Tooltip>
     </li>
 </template>
 
 <script>
+import Tooltip from '@/components/common/Tooltip.vue';
+import { craneProperties } from '@/const/properties';
+import { alg } from '@/utils/alg';
+import WithUnitType from '@/components/common/WithUnitType.vue';
+import IconCrane from '@/components/common/Icons/IconCrane.vue';
+
 export default {
     name: 'ComplexHoldingParametersCrane',
+    components: { IconCrane, WithUnitType, Tooltip },
     props: {
-        crane: Object
-        // id: Number,
-        // title: {
-        //     type: [String]
-        // },
-        // object_id: Number,
-        // photo: Array,
-        // crane_capacity: Number,
-        // crane_type: Number,
-        // crane_location: Number,
-        // crane_beam: Number,
-        // crane_beams_amount: Number,
-        // crane_span: Number,
-        // crane_hoisting: Number,
-        // crane_controls: Array,
-        // crane_hooks: Number,
-        // crane_hook_height: Number,
-        // crane_condition: Number,
-        // crane_supervision: Number,
-        // crane_documents: {
-        //     type: [Number, Array]
-        // },
-        // description: String,
-        // activity: Number,
-        // order_row: Number,
-        // deleted: Number,
-        // publ_time: Number,
-        // last_update: Number,
-        // state: Number,
-        // beam: Number,
-        // beamAmount: Number,
-        // hoisting: Number,
-        // location: {
-        //     id: Number,
-        //     title: String
-        // },
-        // type: {
-        //     id: Number,
-        //     title: String
-        // },
-        // controls: {
-        //     type: [Number, Object, Array]
-        // }
+        crane: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        properties() {
+            const properties = Object.keys(craneProperties).map(key => {
+                const propertiesObject = {
+                    ...craneProperties[key]
+                };
+
+                if (craneProperties[key].condition) {
+                    propertiesObject.value = this.crane[craneProperties[key].condition]
+                        ? alg.extractDeepProperty(this.crane, key)[0]
+                        : null;
+
+                    return propertiesObject;
+                }
+
+                if (craneProperties[key].unitType) {
+                    propertiesObject.value = this.crane[key];
+                } else {
+                    propertiesObject.value =
+                        this.crane[key] === 1 ? 'Да/есть' : this.crane[key] === 0 ? 'Нет' : null;
+                }
+
+                return propertiesObject;
+            });
+
+            return alg.deleteObjectsWithUndueProperties(properties, 'value', 0);
+        },
+        status() {
+            if (this.crane.state && this.crane.state.id > 1) {
+                return this.crane.state.title;
+            }
+
+            return null;
+        }
     }
 };
 </script>
---
+++ src/components/common/Icons/IconCrane.vue
@@ -0,0 +1,26 @@
+<template>
+    <svg
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-miterlimit="10"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 32 32"
+        xml:space="preserve"
+    >
+        <polygon stroke-width="2" points="12,3 12,7 14,10 18,10 20,7 20,3 " />
+        <path
+            stroke-width="2"
+            d="M16,10v3l1.1,2.8c0.4,1.1-0.4,2.2-1.5,2.2h0c-0.9,0-1.6-0.7-1.6-1.6V16"
+        />
+        <rect x="4" y="24" stroke-width="2" width="24" height="5" />
+        <line stroke-width="2" x1="16" y1="18" x2="28" y2="24" />
+        <line stroke-width="2" x1="4" y1="24" x2="16" y2="18" />
+    </svg>
+</template>
+<script>
+export default {
+    name: 'IconCrane'
+};
+</script>
