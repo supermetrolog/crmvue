9fd2a26e669ee5866aec56f5543c3eb989543eb4
nknwns <andrejshaev@gmail.com>
2024-03-03 19:11:33 +0300

    feat: create mapper.js (utility for parsing object properties), improve alg.js

---
+++ src/utils/mapper.js
@@ -0,0 +1,223 @@
+import { alg } from '@/utils/alg';
+import { formatterObject } from '@/plugins';
+
+const propertiesToTableFormat = (object, props) => {
+    return Object.keys(props).reduce((acc, property) => {
+        const currentPropertyObject = props[property];
+
+        // Проверяем, имеют ли свойства условия для вывода
+
+        if (currentPropertyObject.condition && object[currentPropertyObject.condition] !== 1)
+            return acc;
+
+        if (currentPropertyObject.lengthCondition && !object[currentPropertyObject.lengthCondition])
+            return acc;
+
+        // Базовый объект, который отдадим
+        const formattedObject = {
+            name: currentPropertyObject.name
+        };
+
+        if (currentPropertyObject.unitType)
+            formattedObject.unitType = currentPropertyObject.unitType;
+
+        // Обработка свойств в виде min/max
+        if (currentPropertyObject.range) {
+            if (Number(object[`${property}_min`])) {
+                formattedObject.value = formatterObject.numberOrRangeNew(
+                    object[`${property}_min`],
+                    object[`${property}_max`]
+                );
+            } else {
+                formattedObject.value = null;
+            }
+        }
+
+        // Обработка свойств в виде тип-количество ([1, 4, 2, 6] - 4шт. типа 1, 6шт. типа 2)
+        else if (currentPropertyObject.count && currentPropertyObject.types) {
+            if (object[property] instanceof Array && object[property].length) {
+                const pairs = alg.chunk(object[property], 2);
+
+                const types = pairs.reduce((acc, pair) => {
+                    if (pair[0] in acc) acc[pair[0]] += pair[1];
+                    else acc[pair[0]] = pair[1];
+
+                    return acc;
+                }, {});
+
+                formattedObject.value = Object.keys(types).map(key => ({
+                    type: currentPropertyObject.types[key],
+                    value: types[key]
+                }));
+                formattedObject.multiple = true;
+            } else if (object[property] instanceof Object && !(object[property] instanceof Array)) {
+                formattedObject.value = Object.keys(object[property]).map(key => ({
+                    type: currentPropertyObject.types[key],
+                    value: object[property][key]
+                }));
+            } else {
+                formattedObject.value = null;
+            }
+        }
+
+        // Обработка простых свойств со значением количества в виде (power: 3430) в формате value !== 0
+        else if (currentPropertyObject.count && currentPropertyObject.strict) {
+            formattedObject.value = object[property] ? object[property] : null;
+        }
+
+        // Обработка простых свойств со значением количества в виде (power: 3430)
+        else if (currentPropertyObject.count) {
+            formattedObject.value = object[property];
+        }
+
+        // Обработка свойств в виде массива типов ([1, 5, 3] - тип_1, тип_5, тип_3)
+        else if (currentPropertyObject.types && currentPropertyObject.multiple) {
+            if (object[property] instanceof Array && object[property].length) {
+                formattedObject.value = object[property]
+                    .map(element => currentPropertyObject.types[element])
+                    .join(', ');
+            } else {
+                formattedObject.value = null;
+            }
+        }
+
+        // Обработка свойств, где необходимо узнать длину массива
+        else if (currentPropertyObject.arraySize) {
+            if (object[property] instanceof Array) formattedObject.value = object[property].length;
+            else if (object[property] instanceof Object)
+                formattedObject.value = Object.keys(object[property]).length;
+            else formattedObject.value = null;
+        }
+
+        // Обработка самых простых свойств виде свойство:тип
+        else if (currentPropertyObject.types) {
+            formattedObject.value = currentPropertyObject.types[object[property]] ?? null;
+        }
+
+        // Вдруг взялись какие-то непонятные приколыши
+        else {
+            formattedObject.value = object[property] ? 'Да/есть' : null;
+        }
+
+        return [...acc, formattedObject];
+    }, []);
+};
+
+const propertiesToTableFormatWithSections = (object, sections) => {
+    return sections.map((section, index) => {
+        const properties = propertiesToTableFormat(object, section.properties);
+
+        return {
+            id: section.id || `#${index}`,
+            name: section.name,
+            properties: properties
+        };
+    });
+};
+
+const propertiesToParametersFormat = (object, properties) => {
+    const formattedObject = [];
+
+    if (properties.range) {
+        Object.keys(properties.range).forEach(property => {
+            const valueMin = Number(object[`${property}_min`]);
+
+            if (valueMin) {
+                formattedObject.push({
+                    ...properties.range[property],
+                    valueMin: valueMin,
+                    valueMax: Number(object[`${property}_max`]),
+                    range: true
+                });
+            }
+        });
+    }
+
+    if (properties.count) {
+        Object.keys(properties.count).forEach(property => {
+            formattedObject.push({
+                ...properties.count[property],
+                value:
+                    object[property] instanceof Array
+                        ? object[property].length
+                        : object[property] instanceof Object
+                          ? Object.keys(object[property]).length
+                          : object[property],
+                count: true
+            });
+        });
+    }
+
+    if (properties.types) {
+        Object.keys(properties.types).forEach(property => {
+            const currentPropertyObject = properties.types[property];
+            let valueObj = { value: object[property] };
+
+            if (currentPropertyObject.countProperty)
+                valueObj.valueCount = object[currentPropertyObject.countProperty];
+
+            if (currentPropertyObject.withCount) {
+                if (valueObj.value instanceof Array)
+                    valueObj.value = valueObj.value.length ? valueObj.value : null;
+                else if (valueObj instanceof Object)
+                    valueObj.value = Object.keys(valueObj.value).reduce((acc, key) => {
+                        acc.push(key, valueObj.value[key]);
+                        return acc;
+                    }, []);
+            }
+
+            if (!currentPropertyObject.types && valueObj.value == 2) valueObj.value = null;
+
+            formattedObject.push({ ...currentPropertyObject, ...valueObj });
+        });
+    }
+
+    return alg.deleteObjectsWithUndueProperties(formattedObject, 'value', 0);
+};
+
+const propertiesToPrices = (object, properties, isRange = false) => {
+    return Object.keys(properties).reduce((acc, property) => {
+        let currentValueObject = {};
+
+        if (properties[property] instanceof Object)
+            currentValueObject = { ...properties[property] };
+        else currentValueObject.name = properties[property];
+
+        if (isRange) {
+            if (object[property + '_min'] || object[property + '_max']) {
+                currentValueObject.valueMin = object[property + '_min'];
+                currentValueObject.valueMax = object[property + '_max'];
+            }
+        } else {
+            currentValueObject.value = object[property];
+        }
+
+        if (currentValueObject.value || currentValueObject.valueMin || currentValueObject.valueMax)
+            acc.push(currentValueObject);
+
+        return acc;
+    }, []);
+};
+
+const propertiesToRangeFormat = (object, properties) => {
+    return Object.keys(properties).reduce((acc, property) => {
+        let currentValueObject = {
+            name: properties[property].name,
+            valueMin: object[property + '_min'],
+            valueMax: object[property + '_max']
+        };
+
+        if (currentValueObject.valueMin || currentValueObject.valueMax)
+            acc.push(currentValueObject);
+
+        return acc;
+    }, []);
+};
+
+export const mapper = {
+    propertiesToTableFormat,
+    propertiesToTableFormatWithSections,
+    propertiesToParametersFormat,
+    propertiesToPrices,
+    propertiesToRangeFormat
+};
