394d4dfbda02266a98e3320649cbcd4387949b31
nknwns <andrejshaev@gmail.com>
2024-03-03 20:38:04 +0300

    feat: add ComplexPurposes, refactoring ComplexParameters

---
+++ src/components/Complex/ComplexPurposes.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="complex-purposes">
+        <strong
+            v-for="(purpose, key) of preparedPurposes"
+            :key="key"
+            v-tippy="purpose.name"
+            class="complex-purposes__item"
+        >
+            <i :class="purpose.icon"></i>
+        </strong>
+    </div>
+</template>
+<script>
+import { entityOptions } from '@/const/options/options';
+
+export default {
+    name: 'ComplexPurposes',
+    props: {
+        purposes: {
+            type: Array,
+            required: true
+        }
+    },
+    computed: {
+        preparedPurposes() {
+            return this.purposes.map(purpose => entityOptions.object.purposes[purpose]);
+        }
+    }
+};
+</script>
