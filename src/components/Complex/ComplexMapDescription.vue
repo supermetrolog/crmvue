950dd2f286194350bef75789a40264d0d3b154d0
nknwns <andrejshaev@gmail.com>
2024-03-06 11:06:19 +0300

    fix: fix empty fields in crane, fix console warning

--- src/components/Complex/ComplexMapDescription.vue
+++ src/components/Complex/ComplexMapDescription.vue
@@ -100,7 +100,6 @@ export default {
             required: true
         }
     },
-    data() {},
     computed: {
         directionOptions() {
             return entityOptions.location.direction;
