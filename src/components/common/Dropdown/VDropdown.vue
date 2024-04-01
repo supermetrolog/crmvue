d5e2ad47512f77b92610143c6cc82188f306ed00
nknwns <andrejshaev@gmail.com>
2024-04-01 16:22:49 +0300

    chore: add v-dropdown package and create wrapper for v-dropdown (add Escape-close support)

---
+++ src/components/common/Dropdown/VDropdown.vue
@@ -0,0 +1,25 @@
+<template>
+    <ExtDropdown ref="dropdown" @visible-change="visibleHandler">
+        <template #trigger>
+            <slot name="trigger" />
+        </template>
+        <slot />
+    </ExtDropdown>
+</template>
+<script>
+import ExtDropdown from 'v-dropdown';
+export default {
+    name: 'VDropdown',
+    components: { ExtDropdown },
+    methods: {
+        eventListener(event) {
+            event.stopPropagation();
+            if (event.code === 'Escape') this.$refs.dropdown.close();
+        },
+        visibleHandler(visible) {
+            if (visible) document.addEventListener('keydown', this.eventListener, true);
+            else document.removeEventListener('keydown', this.eventListener, true);
+        }
+    }
+};
+</script>
