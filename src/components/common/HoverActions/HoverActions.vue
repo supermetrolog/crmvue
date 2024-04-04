ef4a48607b3b070f5bb84717d3ffffc15b9f4d58
nknwns <andrejshaev@gmail.com>
2024-04-04 10:34:33 +0300

    chore: add HoverActions and skeleton-styles, fix modal, improve scss-mixin

---
+++ src/components/common/HoverActions/HoverActions.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="hover-actions">
+        <div class="hover-actions__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'HoverActions'
+};
+</script>
---
+++ src/components/common/HoverActions/HoverActionsButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button v-tippy="label" class="hover-actions__button">
+        <slot></slot>
+    </button>
+</template>
+<script>
+export default {
+    name: 'HoverActionsButton',
+    props: {
+        label: {
+            type: String,
+            default: null
+        }
+    }
+};
+</script>
