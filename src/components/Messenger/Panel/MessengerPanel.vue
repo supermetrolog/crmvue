0381262c43eef4b0435350125cfae37a1b6c98e9
nknwns <andrejshaev@gmail.com>
2024-04-20 23:33:17 +0300

    feat: create MessengerPanel structure and templates (test-data)

--- src/components/Messenger/Panel/MessengerPanel.vue
+++ src/components/Messenger/Panel/MessengerPanel.vue
@@ -1,8 +1,26 @@
 <template>
-    <div class="messenger-panel"></div>
+    <div class="messenger-panel">
+        <MessengerPanelLoader v-if="isLoading" />
+        <MessengerPanelContent v-else-if="hasPanel" />
+        <MessengerPanelEmpty v-else />
+    </div>
 </template>
 <script>
+import MessengerPanelContent from '@/components/Messenger/Panel/MessengerPanelContent.vue';
+import MessengerPanelEmpty from '@/components/Messenger/Panel/MessengerPanelEmpty.vue';
+import MessengerPanelLoader from '@/components/Messenger/Panel/MessengerPanelLoader.vue';
+import { mapState } from 'vuex';
+import { LoaderMixin } from '@/components/Messenger/loader.mixin';
+
 export default {
-    name: 'MessengerPanel'
+    name: 'MessengerPanel',
+    components: { MessengerPanelLoader, MessengerPanelEmpty, MessengerPanelContent },
+    mixins: [LoaderMixin],
+    computed: {
+        ...mapState({
+            hasPanel: state => state.Messenger.currentAsideDialogID,
+            originalLoader: state => state.Messenger.loadingPanel
+        })
+    }
 };
 </script>
