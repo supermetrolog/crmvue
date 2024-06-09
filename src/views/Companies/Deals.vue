3129a6e3ca21cc221df56f749c5db5accc7020cb
nknwns <andrejshaev@gmail.com>
2024-06-09 21:19:08 +0300

    fix: fix deals import

--- src/views/Companies/Deals.vue
+++ src/views/Companies/Deals.vue
@@ -1,35 +1,9 @@
 <template>
-    <div class="companies-deals">
-        <div class="row">
-            <div class="col-12">
-                <div class="objects">
-                    <!-- <ObjectsSearch @search="search" class="mb-2" /> -->
-                    <CompanyObjectsList
-                        :objects="allObjects"
-                        :current-objects="[]"
-                        :selected-objects="[]"
-                        :disabled="true"
-                        :loader="allObjectsLoader"
-                        :view-mode="viewMode"
-                        col="col-2"
-                    />
-                    <Pagination @loadMore="loadMore" :pagination="pagination" class="text-center" />
-                </div>
-            </div>
-        </div>
-    </div>
+    <div class="companies-deals"></div>
 </template>
 
 <script>
-import { MixinAllObject } from '@/components/Company/Object/mixins';
-
 export default {
-    name: 'CompaniesDeals',
-    mixins: [MixinAllObject],
-    methods: {
-        async getPreventStepObjects() {}
-    }
+    name: 'CompaniesDeals'
 };
 </script>
-
-<style></style>
