5f143eb316cd54f3438d19bc573c34419d94fa48
nknwns <andrejshaev@gmail.com>
2024-07-09 13:01:11 +0300

    chore: improve Header

--- src/components/Header/HeaderActions.vue
+++ src/components/Header/HeaderActions.vue
@@ -1,39 +1,19 @@
 <template>
-    <teleport to="body">
-        <FormCompanyDeal v-if="dealFormVisible" @close="clickCloseDealForm" />
-    </teleport>
     <div class="header__actions">
-        <Button @click="clickOpenDealForm" small icon>
-            создать сделку
+        <teleport to="body">
+            <FormCompanyDeal v-if="dealFormIsVisible" @close="dealFormIsVisible = false" />
+        </teleport>
+        <Button @click="dealFormIsVisible = true" small icon>
+            Создать сделку
             <i class="ml-2 fa-solid fa-bolt"></i>
         </Button>
     </div>
 </template>
 
-<script>
+<script setup>
 import FormCompanyDeal from '@/components/Forms/Company/FormCompanyDeal.vue';
 import Button from '@/components/common/Button.vue';
+import { shallowRef } from 'vue';
 
-export default {
-    name: 'HeaderActions',
-    components: {
-        Button,
-        FormCompanyDeal
-    },
-    data() {
-        return {
-            dealFormVisible: false
-        };
-    },
-    methods: {
-        clickOpenDealForm() {
-            this.dealFormVisible = true;
-        },
-        clickCloseDealForm() {
-            this.dealFormVisible = false;
-        }
-    }
-};
+const dealFormIsVisible = shallowRef(false);
 </script>
-
-<style></style>
--- src/components/Header/HeaderFunctions.vue
+++ src/components/Header/HeaderFunctions.vue
@@ -1,90 +1,77 @@
 <template>
     <teleport to="body">
-        <FormCompanyDeal v-if="isFormDealVisible" @close="isFormDealVisible = false" />
+        <FormCompanyDeal v-if="dealFormIsVisible" @close="dealFormIsVisible = false" />
     </teleport>
-    <ul class="nav-list horizontal">
-        <li ref="calls" class="nav-item notification" :class="{ active: callsVisible }">
-            <a @click.prevent="clickCalls" class="nav-link">
-                <div class="nav-link__content" :class="{ 'current-call-exist': CURRENT_CALLS.length }">
-                    <i class="header__icon fa-solid fa-phone"></i>
-                    <span
-                        v-if="CALLS_COUNT != 0"
-                        class="badge badge-danger"
-                        :class="{ 'badge-info': CURRENT_CALLS.length }"
-                    >
-                        {{ +CALLS_COUNT }}
-                    </span>
-                </div>
-            </a>
-            <AnimationTransition>
-                <HeaderCalls v-if="callsVisible" />
-            </AnimationTransition>
-        </li>
-
-        <li ref="notification" class="nav-item notification" :class="{ active: notificationsVisible }">
-            <a @click.prevent="clickNotification" class="nav-link">
-                <div class="nav-link__content">
-                    <i class="header__icon fa-regular fa-bell"></i>
-                    <span v-if="NOTIFICATIONS_COUNT != 0" class="badge badge-danger">
-                        {{ NOTIFICATIONS_COUNT }}
-                    </span>
-                </div>
-            </a>
-            <AnimationTransition>
-                <HeaderNotifications v-if="notificationsVisible" />
-            </AnimationTransition>
-        </li>
-    </ul>
+    <div class="header__functions">
+        <HoverActionsButton
+            ref="callsButton"
+            @click="callsIsVisible = !callsIsVisible"
+            :active="callsIsVisible"
+            class="header__button"
+            :class="{ 'current-call-exist': CURRENT_CALLS.length }"
+        >
+            <i class="header__icon fa-solid fa-phone"></i>
+            <span
+                v-if="CALLS_COUNT !== 0"
+                class="header__badge badge badge-danger"
+                :class="{ 'badge-info': CURRENT_CALLS.length }"
+            >
+                {{ CALLS_COUNT }}
+            </span>
+        </HoverActionsButton>
+        <AnimationTransition>
+            <HeaderCalls v-if="callsIsVisible" v-on-click-outside="callsClickOutside" />
+        </AnimationTransition>
+        <HoverActionsButton
+            ref="notificationsButton"
+            @click="notificationsIsVisible = !notificationsIsVisible"
+            :active="notificationsIsVisible"
+            class="header__button"
+        >
+            <i class="header__icon fa-regular fa-bell"></i>
+            <span v-if="NOTIFICATIONS_COUNT !== 0" class="header__badge badge badge-danger">
+                {{ NOTIFICATIONS_COUNT }}
+            </span>
+        </HoverActionsButton>
+        <AnimationTransition>
+            <HeaderNotifications
+                v-if="notificationsIsVisible"
+                v-on-click-outside="notificationsClickOutside"
+            />
+        </AnimationTransition>
+    </div>
 </template>
-<script>
+<script setup>
+import { vOnClickOutside } from '@vueuse/components';
 import FormCompanyDeal from '@/components/Forms/Company/FormCompanyDeal.vue';
 import HeaderCalls from '@/components/Header/HeaderCalls.vue';
 import HeaderNotifications from '@/components/Header/HeaderNotifications.vue';
-import { mapActions, mapGetters } from 'vuex';
+import { useStore } from 'vuex';
 import AnimationTransition from '@/components/common/AnimationTransition.vue';
+import { computed, onBeforeMount, ref, shallowRef } from 'vue';
+import HoverActionsButton from '@/components/common/HoverActions/HoverActionsButton.vue';
 
-export default {
-    name: 'HeaderFunctions',
-    components: {
-        AnimationTransition,
-        FormCompanyDeal,
-        HeaderCalls,
-        HeaderNotifications
-    },
-    data() {
-        return {
-            isFormDealVisible: false,
-            callsVisible: false,
-            notificationsVisible: false
-        };
-    },
-    computed: {
-        ...mapGetters(['NOTIFICATIONS_COUNT', 'CALLS_COUNT', 'CURRENT_CALLS'])
-    },
-    methods: {
-        ...mapActions(['FETCH_NOTIFICATIONS_COUNT', 'FETCH_CALLS_COUNT']),
-        clickNotification() {
-            this.notificationsVisible = !this.notificationsVisible;
-        },
-        clickCalls() {
-            this.callsVisible = !this.callsVisible;
-        },
-        close(e) {
-            if (!this.$refs.notification.contains(e.target)) {
-                this.notificationsVisible = false;
-            }
-            if (!this.$refs.calls.contains(e.target)) {
-                this.callsVisible = false;
-            }
-        }
-    },
-    mounted() {
-        this.FETCH_NOTIFICATIONS_COUNT();
-        this.FETCH_CALLS_COUNT();
-        document.addEventListener('click', this.close);
-    },
-    beforeUnmount() {
-        document.removeEventListener('click', this.close);
-    }
-};
+const store = useStore();
+
+const callsButton = ref(null);
+const notificationsButton = ref(null);
+
+const dealFormIsVisible = shallowRef(false);
+const callsIsVisible = shallowRef(false);
+const notificationsIsVisible = shallowRef(false);
+
+const NOTIFICATIONS_COUNT = computed(() => store.getters.NOTIFICATIONS_COUNT);
+const CALLS_COUNT = computed(() => store.getters.CALLS_COUNT);
+const CURRENT_CALLS = computed(() => store.getters.CURRENT_CALLS);
+
+const callsClickOutside = [() => (callsIsVisible.value = false), { ignore: [callsButton] }];
+const notificationsClickOutside = [
+    () => (notificationsIsVisible.value = false),
+    { ignore: [notificationsButton] }
+];
+
+onBeforeMount(() => {
+    store.dispatch('FETCH_NOTIFICATIONS_COUNT');
+    store.dispatch('FETCH_CALLS_COUNT');
+});
 </script>
