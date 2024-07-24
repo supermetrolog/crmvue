321779d58ce83fd5fc4e7ea8181b8daf7a421e01
nknwns <andrejshaev@gmail.com>
2024-07-24 03:00:20 +0300

    feat(Carousel): now slides in Carousel create gallery (if carousel has many photos)

--- src/components/common/Carousel.vue
+++ src/components/common/Carousel.vue
@@ -1,15 +1,38 @@
 <template>
-    <div class="carousel">
+    <div ref="carousel" class="carousel">
         <div :class="{ carousel__scroll: !grid, carousel__grid: grid }">
             <VLazyImage
-                v-for="(item, idx) in slides"
+                v-for="(item, idx) in photos"
                 :key="item.src"
                 @click="openModal(idx)"
                 class="carousel__item"
+                :style="{ 'flex-basis': flexBasis + '%' }"
                 :src="item.src"
             />
+            <div v-if="slides.length > count" class="carousel__item carousel__gallery">
+                <VLazyImage
+                    v-for="(item, key) in gallery"
+                    :key="key"
+                    @click="openModal(count + key)"
+                    class="carousel__cell"
+                    :src="item.src ?? $url.api.fileNotFound()"
+                />
+                <div
+                    v-if="slides.length > count + 4"
+                    @click.prevent="openModal(count + 4)"
+                    class="carousel__helper"
+                >
+                    <p>Ещё</p>
+                    <p>{{ slides.length - count - 4 }} фото</p>
+                </div>
+            </div>
             <template v-if="!slides.length">
-                <NoImage v-for="key in 3" :key="key" class="carousel__item" />
+                <NoImage
+                    v-for="key in count + 1"
+                    :key="key"
+                    :style="{ 'flex-basis': flexBasis + '%' }"
+                    class="carousel__item"
+                />
             </template>
         </div>
         <Modal v-if="modalIsOpen" @close="closeModal" class="carousel__modal" :title="modalTitle">
@@ -60,7 +83,9 @@ import Modal from '@/components/common/Modal.vue';
 import { VueAgile } from 'vue-agile';
 import VLazyImage from 'v-lazy-image';
 import NoImage from '@/components/common/NoImage.vue';
-import { computed, shallowRef } from 'vue';
+import { computed, onMounted, ref, shallowRef } from 'vue';
+
+const IMAGE_SIZE = 325;
 
 const props = defineProps({
     slides: {
@@ -92,8 +117,24 @@ const listOptions = {
 
 const modalIsOpen = shallowRef(false);
 const currentSlideIndex = shallowRef(0);
+const carousel = ref(null);
+const count = shallowRef(0);
+const flexBasis = shallowRef(null);
 
-const modalTitle = computed(() => 'Просмотр изображений' + (props.title ?? ''));
+const modalTitle = computed(() => 'Просмотр изображений. ' + (props.title ?? ''));
+const photos = computed(() => {
+    if (props.slides.length <= count.value) return props.slides;
+    return props.slides.slice(0, count.value);
+});
+const gallery = computed(() => {
+    const _gallery = props.slides.slice(count.value, count.value + 4);
+
+    if (_gallery.length < 4)
+        // eslint-disable-next-line no-unused-vars
+        _gallery.push(...Array.from(Array(4 - _gallery.length).keys()).map(_ => ({ src: null })));
+
+    return _gallery;
+});
 
 const openModal = id => {
     modalIsOpen.value = true;
@@ -104,4 +145,18 @@ const closeModal = () => {
     currentSlideIndex.value = 0;
     modalIsOpen.value = false;
 };
+
+const createGallery = () => {
+    const width = carousel.value.clientWidth;
+    console.log(width);
+    const _count = Math.floor(width / IMAGE_SIZE);
+    flexBasis.value = 100 / _count;
+
+    if (_count === props.slides.length) count.value = _count;
+    else count.value = _count - 1;
+};
+
+onMounted(() => {
+    createGallery();
+});
 </script>
