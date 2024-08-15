6a0b76453a93163ac18864d9181a14f93a56bc79
nknwns <andrejshaev@gmail.com>
2024-08-15 19:28:08 +0700

    feat(forms): now CheckboxChip may contain button with icons, add validation color for RadioOptions, new size mode for Loader

--- src/components/common/Loader.vue
+++ src/components/common/Loader.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="loader">
+    <div class="loader" :class="{ small: small }">
         <div class="loader__container">
-            <Spinner />
+            <Spinner :class="{ small: small }" />
         </div>
     </div>
 </template>
 
 <script setup>
 import Spinner from '@/components/common/Spinner.vue';
+
+defineProps({ small: { type: Boolean, default: false } });
 </script>
