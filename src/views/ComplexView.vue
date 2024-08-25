8675849809851680d87da673803d831528262b88
nknwns <andrejshaev@gmail.com>
2024-08-25 23:39:11 +0700

    [#349] feat: now Complex shows object coords if its own coords are missing; migrate to Composition API

--- src/views/ComplexView.vue
+++ src/views/ComplexView.vue
@@ -1,125 +1,120 @@
 <template>
-    <div v-if="complex" class="ComplexView container-fluid">
-        <teleport to="body">
-            <FormComplex
-                v-if="complexFormModalVisible"
-                @close="toggleComplexFormModalVisible"
-                :complex="complex"
-            />
-            <FormModalDelete
-                v-if="complexFormDeleteModalVisible"
-                @close="toggleComplexDeleteFormModalVisible"
-                @delete="deleteComplex(complex.id)"
-                text="комплекс"
-                :title="`Удаление комплекса #${complex.id} (${complex.title || 'Без названия'})`"
-            />
-            <ComplexPhotoDownloader ref="downloader" />
-        </teleport>
-        <div class="ComplexHeader">
-            <div class="ComplexHeader-content">
-                <h1 class="ComplexHeader-name">{{ complex.title || 'Нет названия' }}</h1>
-                <span class="ComplexHeader-description">
-                    <strong>ID комплекса {{ complex.id }}, </strong>
-                    поступление {{ $formatter.date(complex.publ_time, true) }},
-                    <span v-if="complex.author">
-                        {{ complex.author.userProfile.full_name }}
+    <div class="ComplexView container-fluid">
+        <Loader v-if="isLoading" center />
+        <template v-else-if="complex">
+            <teleport to="body">
+                <FormComplex
+                    v-if="formIsVisible"
+                    @close="formIsVisible = false"
+                    :complex="complex"
+                />
+                <ComplexPhotoDownloader ref="downloader" />
+            </teleport>
+            <div class="ComplexHeader">
+                <div class="ComplexHeader-content">
+                    <h1 class="ComplexHeader-name">{{ complex.title || 'Нет названия' }}</h1>
+                    <span class="ComplexHeader-description">
+                        <strong>ID комплекса {{ complex.id }}, </strong>
+                        поступление {{ formattedDate }},
+                        <span v-if="complex.author">
+                            {{ complex.author.userProfile.full_name }}
+                        </span>
+                        <span v-else>без автора</span>, последнее обновление
+                        {{ updatedDate }}
                     </span>
-                    <span v-else>без автора</span>, последнее обновление
-                    {{ $formatter.date(complex.last_update, true) }}
-                </span>
+                </div>
+                <div class="complex-header__actions">
+                    <a class="button button--icon" :href="oldComplexUrl" target="_blank">
+                        <span>Перейти на старую версию</span>
+                        <i class="fas fa-eye edit"></i>
+                    </a>
+                    <template v-if="editAccess">
+                        <Button @click="formIsVisible = true" icon>
+                            <span>Редактировать</span>
+                            <i class="fas fa-pen edit"></i>
+                        </Button>
+                        <Button @click="deleteComplex" danger icon>
+                            <span>Удалить</span>
+                            <i class="fas fa-trash edit"></i>
+                        </Button>
+                    </template>
+                </div>
             </div>
-            <div class="complex-header__actions">
-                <a
-                    class="button button--icon"
-                    :href="oldComplexUrl"
-                    target="_blank
-"
-                >
-                    <span>Перейти на старую версию</span>
-                    <i class="fas fa-eye edit"></i>
-                </a>
-                <template v-if="editAccess">
-                    <Button @click="toggleComplexFormModalVisible" icon>
-                        <span>Редактировать</span>
-                        <i class="fas fa-pen edit"></i>
-                    </Button>
-                    <Button @click="toggleComplexDeleteFormModalVisible" danger icon>
-                        <span>Удалить</span>
-                        <i class="fas fa-trash edit"></i>
-                    </Button>
-                </template>
-            </div>
-        </div>
-        <ComplexMap :location="complexLocation" />
-        <ComplexAbout :complex="complex" />
-        <ComplexHoldings :objects="complex.objects" />
+            <ComplexMap :location="location" />
+            <ComplexAbout :complex="complex" />
+            <ComplexHoldings :objects="complex.objects" />
+        </template>
+        <EmptyData v-else>Комплекс не найден..</EmptyData>
     </div>
 </template>
 
-<script>
-import { mapActions, mapState } from 'vuex';
+<script setup>
+import { useStore } from 'vuex';
 import ComplexAbout from '@/components/Complex/ComplexAbout.vue';
 import ComplexMap from '@/components/Complex/ComplexMap.vue';
 import ComplexHoldings from '@/components/Complex/Holding/ComplexHoldings.vue';
 import Button from '@/components/common/Button.vue';
 import FormComplex from '@/components/Forms/Complex/FormComplex.vue';
-import FormModalDelete from '@/components/Forms/FormModalDelete.vue';
 import ComplexPhotoDownloader from '@/components/Complex/ComplexPhotoDownloader.vue';
+import { computed, onBeforeMount, provide, shallowRef } from 'vue';
+import { getLinkComplexOld } from '@/utils/url.js';
+import { useRoute, useRouter } from 'vue-router';
+import { toDateFormat } from '@/utils/formatter.js';
+import { useConfirm } from '@/composables/useConfirm.js';
+import EmptyData from '@/components/common/EmptyData.vue';
+import Loader from '@/components/common/Loader.vue';
 
-export default {
-    name: 'ComplexView',
-    components: {
-        ComplexPhotoDownloader,
-        FormModalDelete,
-        FormComplex,
-        Button,
-        ComplexHoldings,
-        ComplexMap,
-        ComplexAbout
-    },
-    provide() {
-        return {
-            openDownloader: (photos, zipName = null) => this.$refs.downloader.open(photos, zipName)
-        };
-    },
-    data() {
-        return {
-            editAccess: true,
-            complexFormModalVisible: false,
-            complexFormDeleteModalVisible: false,
-            downloaderPhotos: []
-        };
-    },
-    computed: {
-        ...mapState({
-            complex: state => state.Complex.complex
-        }),
-        complexLocation() {
-            return {
-                ...this.complex.location,
-                fromMkad: this.complex.from_mkad,
-                latitude: this.complex.latitude,
-                longitude: this.complex.longitude
-            };
-        },
-        oldComplexUrl() {
-            return this.$url.complexOld(this.complex.id, this.$route.query.offer_id);
-        }
-    },
-    methods: {
-        ...mapActions({
-            fetchComplex: 'Complex/FETCH_COMPLEX',
-            deleteComplex: 'Complex/DELETE_COMPLEX'
-        }),
-        toggleComplexFormModalVisible() {
-            this.complexFormModalVisible = !this.complexFormModalVisible;
-        },
-        toggleComplexDeleteFormModalVisible() {
-            this.complexFormDeleteModalVisible = !this.complexFormDeleteModalVisible;
-        }
-    },
-    async created() {
-        await this.fetchComplex(this.$route.params.complex_id);
+const store = useStore();
+const route = useRoute();
+const router = useRouter();
+const { confirm } = useConfirm();
+
+const isLoading = shallowRef(false);
+const downloader = shallowRef(null);
+const editAccess = shallowRef(true);
+const formIsVisible = shallowRef(false);
+
+provide('openDownloader', (photos, zipName = null) => downloader.value.open(photos, zipName));
+
+const complex = computed(() => store.state.Complex.complex);
+const oldComplexUrl = computed(() => getLinkComplexOld(complex.value.id, route.query.offer_id));
+const location = computed(() => {
+    const _location = {
+        ...complex.value.location,
+        fromMkad: complex.value.from_mkad
+    };
+
+    if (complex.value.latitude) {
+        _location.latitude = complex.value.latitude;
+        _location.longitude = complex.value.longitude;
+    } else if (complex.value.objects?.length) {
+        _location.latitude = complex.value.objects[0].latitude;
+        _location.longitude = complex.value.objects[0].longitude;
+    }
+
+    return _location;
+});
+
+const formattedDate = computed(() =>
+    toDateFormat(complex.value.publ_time * 1000, 'D MMMM YYYY г., в HH:mm')
+);
+const updatedDate = computed(() =>
+    toDateFormat(complex.value.last_update * 1000, 'D MMMM YYYY г., в HH:mm')
+);
+
+const deleteComplex = async () => {
+    const confirmed = await confirm('Вы действительно хотите безвозвратно удалить комплекс?');
+    if (!confirmed) return;
+
+    const deleted = await store.dispatch('Complex/DELETE_COMPLEX', complex.value.id);
+    if (deleted) {
+        await router.push({ name: 'OffersMain' });
     }
 };
+
+onBeforeMount(async () => {
+    isLoading.value = true;
+    await store.dispatch('Complex/FETCH_COMPLEX', route.params.complex_id);
+    isLoading.value = false;
+});
 </script>
