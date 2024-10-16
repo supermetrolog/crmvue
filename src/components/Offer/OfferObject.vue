b522a9ff748494f2d750d78ca5b92b1c4bfd1d09
nknwns <andrejshaev@gmail.com>
2024-10-16 15:17:57 +0700

    [#404] refactor: migrate yandex map offers to Composition API, fix problems, improve UI, add loaders and more info about map dots

--- src/components/Offer/OfferObject.vue
+++ src/components/Offer/OfferObject.vue
@@ -1,14 +1,27 @@
 <template>
     <AnimationTransition :speed="0.5" :animation="{ enter: 'slideInLeft', leave: 'slideOutLeft' }">
-        <div v-if="offers.length" class="object-view scroller">
-            <HoverActionsButton @click="close" label="Закрыть панель" class="object-view__close">
-                <i class="fas fa-times"></i>
-            </HoverActionsButton>
-            <Spinner v-if="isLoading" />
-            <div v-else>
-                <DashboardChip class="dashboard-bg-gray-l w-100 text-center mb-2">
-                    Предложений: {{ offers.length }}
+        <div v-if="isShowing" class="object-view scroller">
+            <div class="object-view__header">
+                <DashboardChip class="dashboard-bg-gray-l w-100 text-center">
+                    <span>Просмотр предложений: </span>
+                    <span v-if="isLoadingOffers || isLoadingObject">загрузка..</span>
+                    <span v-else>{{ offers?.length ?? 0 }}</span>
                 </DashboardChip>
+                <HoverActionsButton
+                    @click="close"
+                    label="Закрыть панель"
+                    class="object-view__close"
+                >
+                    <i class="fas fa-times"></i>
+                </HoverActionsButton>
+            </div>
+            <hr />
+            <Spinner
+                v-if="isLoadingObject || isLoadingOffers"
+                class="absolute-center"
+                :label="spinnerLabel"
+            />
+            <div v-else>
                 <div class="row no-gutters">
                     <CompanyObjectItemOfferOnly
                         v-for="offer in offers"
@@ -22,70 +35,73 @@
     </AnimationTransition>
 </template>
 
-<script>
-import { mapActions } from 'vuex';
+<script setup>
+import { useStore } from 'vuex';
 import CompanyObjectItemOfferOnly from '@/components/Company/Object/CompanyObjectItemOfferOnly.vue';
 import AnimationTransition from '@/components/common/AnimationTransition.vue';
 import Spinner from '@/components/common/Spinner.vue';
 import { useDelayedLoader } from '@/composables/useDelayedLoader.js';
 import DashboardChip from '@/components/Dashboard/DashboardChip.vue';
 import HoverActionsButton from '@/components/common/HoverActions/HoverActionsButton.vue';
+import { computed, ref, shallowRef, watch } from 'vue';
 
-export default {
-    name: 'OfferObject',
-    components: {
-        HoverActionsButton,
-        DashboardChip,
-        Spinner,
-        AnimationTransition,
-        CompanyObjectItemOfferOnly
-    },
-    props: {
-        offerIds: {
-            type: Array,
-            default: () => []
-        }
-    },
-    setup() {
-        const { isLoading } = useDelayedLoader();
-        return { isLoading };
-    },
-    data() {
-        return {
-            offers: []
-        };
-    },
-    watch: {
-        offerIds() {
-            this.fetchOffers();
-        }
-    },
-    methods: {
-        ...mapActions(['SEARCH_OFFERS']),
-        close() {
-            this.offers = [];
-        },
-        async fetchOffers() {
-            this.isLoading = true;
+const props = defineProps({
+    offerIds: {
+        type: Array,
+        default: () => []
+    }
+});
 
-            if (!this.offerIds.length) return;
+const store = useStore();
+const isLoadingOffers = shallowRef(false);
+const { isLoading: isLoadingObject } = useDelayedLoader();
 
-            const { data: offers } = await this.SEARCH_OFFERS({
-                query: {
-                    id: this.offerIds
-                }
-            });
+const offers = ref([]);
+const isShowing = shallowRef(false);
 
-            const query = {
-                object_id: offers.map(el => el.object_id),
-                expand: 'generalOffersMix,contact.emails,contact.phones,object,company.mainContact.phones,company.mainContact.emails,offer,consultant.userProfile'
-            };
-
-            const { data: allOffers } = await this.SEARCH_OFFERS({ query });
-            this.offers = allOffers;
-
-            this.isLoading = false;
+watch(
+    () => props.offerIds,
+    () => {
+        if (props.offerIds?.length) {
+            isShowing.value = true;
+            fetchOffers();
         }
     }
+);
+
+const close = () => {
+    isShowing.value = false;
+    offers.value = [];
+};
+
+const spinnerLabel = computed(() => {
+    if (isLoadingObject.value) return 'Поиск объекта..';
+    if (isLoadingOffers.value) return 'Загрузка предложений..';
+    return null;
+});
+
+const fetchObjects = async () => {
+    const response = await store.dispatch('SEARCH_OFFERS', { query: { id: props.offerIds[0] } });
+    return response.data;
+};
+
+const fetchOffers = async () => {
+    isLoadingObject.value = true;
+
+    const objects = await fetchObjects();
+    if (!objects) return;
+
+    isLoadingOffers.value = true;
+    isLoadingObject.value = false;
+
+    const query = {
+        object_id: objects.map(el => el.object_id),
+        expand: 'generalOffersMix,contact.emails,contact.phones,object,company.mainContact.phones,company.mainContact.emails,offer,consultant.userProfile'
+    };
+
+    const response = await store.dispatch('SEARCH_OFFERS', { query });
+    offers.value = response.data;
+
+    isLoadingOffers.value = false;
 };
 </script>
--- src/components/common/Spinner.vue
+++ src/components/common/Spinner.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="spinner" :class="{ center: center }">
         <div class="spinner__clip"></div>
+        <p v-if="label" class="spinner__label">{{ label }}</p>
     </div>
 </template>
 <script setup>
-defineProps({ center: { type: Boolean, default: false } });
+defineProps({
+    center: { type: Boolean, default: false },
+    label: { type: String, default: null }
+});
 </script>
