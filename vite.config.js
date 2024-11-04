6df4aef621a402890193a09abdbdb57846ccf804
nknwns <andrejshaev@gmail.com>
2024-11-05 06:56:41 +0700

    feat: add splash-screen, add vite plugin in deps

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { htmlInjectionPlugin } from 'vite-plugin-html-injection';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        htmlInjectionPlugin({
+            injections: [
+                {
+                    name: 'Splash Screen',
+                    path: './src/injections/splash-screen.html',
+                    injectTo: 'body'
+                }
+            ]
+        })
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
