df279c151768d221576c16ddfd711a3e95111def
nknwns <andrejshaev@gmail.com>
2024-11-05 06:50:10 +0700

    feat: add user activity system (info about online, tracking activity for current user)

---
+++ src/components/Header/HeaderActivity.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="comments-panel">
+        <div class="comments-panel__content" :class="{ loading: isLoading }">
+            <div class="row heading no-gutters">
+                <div class="title text-left align-self-center">
+                    <p>Активность сотрудников</p>
+                </div>
+            </div>
+            <Loader v-if="isLoading" />
+            <div v-else class="header-activity">
+                <p class="header-activity__label">Сотрудники в сети ({{ onlineUsers.length }}):</p>
+                <div class="header-activity__list">
+                    <UserPreview v-for="user in onlineUsers" :key="user.id" :user="user" />
+                </div>
+                <hr />
+                <p class="header-activity__label">
+                    Сотрудники не в сети ({{ offlineUsers.length }}):
+                </p>
+                <div class="header-activity__list">
+                    <UserPreview v-for="user in offlineUsers" :key="user.id" :user="user" />
+                    <UserPreview v-for="user in unknownUsers" :key="user.id" :user="user" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Loader from '@/components/common/Loader.vue';
+import { computed, onMounted, ref } from 'vue';
+import api from '@/api/api.js';
+import dayjs from 'dayjs';
+import UserPreview from '@/components/User/UserPreview.vue';
+
+const isLoading = ref(true);
+const users = ref([]);
+
+const onlineUsers = computed(() => users.value.filter(user => user.is_online));
+const offlineUsers = computed(() =>
+    users.value
+        .filter(user => !user.is_online && user.last_seen)
+        .sort((a, b) => dayjs(b.last_seen).diff(dayjs(a.last_seen), 'seconds'))
+);
+const unknownUsers = computed(() => users.value.filter(user => !user.last_seen));
+
+const fetchUsers = async () => {
+    isLoading.value = true;
+
+    users.value = await api.user.list();
+
+    isLoading.value = false;
+};
+
+onMounted(() => {
+    fetchUsers();
+});
+</script>
