55befafbde1786cb1594a1f08b0917fe6db0e7eb
nknwns <andrejshaev@gmail.com>
2024-11-13 03:09:50 +0700

    refactor: small refactoring OfferTableItemRelationSelect

--- src/components/Offer/TableItem/OfferTableItemRelationSelect.vue
+++ src/components/Offer/TableItem/OfferTableItemRelationSelect.vue
@@ -1,103 +1,52 @@
 <template>
     <div class="offer-table-item__relations h-100">
-        <button
+        <OfferTableItemRelationSelectElement
             v-if="hasRentOffers"
-            v-tippy="{
-                content:
-                    currentTab === dealOptions.typeStatement.RENT
-                        ? 'Нажмите, чтобы скрыть предложения снизу.'
-                        : 'По этому объекту имеются предложения по аренде. Нажмите, чтобы просмотреть их.',
-                hideOnClick: false
-            }"
-            @click="$emit('open', dealOptions.typeStatement.RENT)"
-            class="offer-table-item__relation-button offer-table-item__relation-button--rent"
-            :class="{ active: currentTab === dealOptions.typeStatement.RENT }"
+            @open="$emit('open', $event)"
+            :active="currentTab === dealOptions.typeStatement.RENT"
+            :type="dealOptions.typeStatement.RENT"
+            label="По этому объекту имеются предложения по аренде"
+            class="offer-table-item__relation-button--rent"
         >
-            <i class="fa-solid fa-caret-up"></i>
-            <span>Аренда</span>
-            <i
-                class="fa-solid offer-table-item__relation-icon"
-                :class="{
-                    'fa-eye-slash': currentTab === dealOptions.typeStatement.RENT,
-                    'fa-eye': currentTab !== dealOptions.typeStatement.RENT
-                }"
-            />
-        </button>
-        <button
+            Аренда
+        </OfferTableItemRelationSelectElement>
+        <OfferTableItemRelationSelectElement
             v-if="hasSaleOffers"
-            v-tippy="{
-                content:
-                    currentTab === dealOptions.typeStatement.SALE
-                        ? 'Нажмите, чтобы скрыть предложения снизу.'
-                        : 'По этому объекту имеются предложения по продаже. Нажмите, чтобы просмотреть их.',
-                hideOnClick: false
-            }"
-            @click="$emit('open', dealOptions.typeStatement.SALE)"
-            class="offer-table-item__relation-button offer-table-item__relation-button--sale"
-            :class="{ active: currentTab === dealOptions.typeStatement.SALE }"
+            @open="$emit('open', $event)"
+            :active="currentTab === dealOptions.typeStatement.SALE"
+            :type="dealOptions.typeStatement.SALE"
+            label="По этому объекту имеются предложения по продаже"
+            class="offer-table-item__relation-button--sale"
         >
-            <i class="fa-solid fa-caret-up"></i>
-            <span>Продажа</span>
-            <i
-                class="fa-solid offer-table-item__relation-icon"
-                :class="{
-                    'fa-eye-slash': currentTab === dealOptions.typeStatement.SALE,
-                    'fa-eye': currentTab !== dealOptions.typeStatement.SALE
-                }"
-            />
-        </button>
-        <button
+            Продажа
+        </OfferTableItemRelationSelectElement>
+        <OfferTableItemRelationSelectElement
             v-if="hasStorageOffers"
-            v-tippy="{
-                content:
-                    currentTab === dealOptions.typeStatement.STORAGE
-                        ? 'Нажмите, чтобы скрыть предложения снизу.'
-                        : 'По этому объекту имеются предложения по ответ.хранению. Нажмите, чтобы просмотреть их.',
-                hideOnClick: false
-            }"
-            @click="$emit('open', dealOptions.typeStatement.STORAGE)"
-            class="offer-table-item__relation-button offer-table-item__relation-button--storage"
-            :class="{ active: currentTab === dealOptions.typeStatement.STORAGE }"
+            @open="$emit('open', $event)"
+            :active="currentTab === dealOptions.typeStatement.STORAGE"
+            :type="dealOptions.typeStatement.STORAGE"
+            label="По этому объекту имеются предложения по ответ.хранению"
+            class="offer-table-item__relation-button--storage"
         >
-            <i class="fa-solid fa-caret-up"></i>
-            <span>Ответ.хран</span>
-            <i
-                class="fa-solid offer-table-item__relation-icon"
-                :class="{
-                    'fa-eye-slash': currentTab === dealOptions.typeStatement.STORAGE,
-                    'fa-eye': currentTab !== dealOptions.typeStatement.STORAGE
-                }"
-            />
-        </button>
-        <button
+            Отв.хран.
+        </OfferTableItemRelationSelectElement>
+        <OfferTableItemRelationSelectElement
             v-if="hasSubleaseOffers"
-            v-tippy="{
-                content:
-                    currentTab === dealOptions.typeStatement.SUBLEASE
-                        ? 'Нажмите, чтобы скрыть предложения снизу.'
-                        : 'По этому объекту имеются предложения по субаренде. Нажмите, чтобы просмотреть их.',
-                hideOnClick: false
-            }"
-            @click="$emit('open', dealOptions.typeStatement.SUBLEASE)"
-            class="offer-table-item__relation-button offer-table-item__relation-button--sublease"
-            :class="{ active: currentTab === dealOptions.typeStatement.SUBLEASE }"
+            @open="$emit('open', $event)"
+            :active="currentTab === dealOptions.typeStatement.SUBLEASE"
+            :type="dealOptions.typeStatement.SUBLEASE"
+            label="По этому объекту имеются предложения по субаренде"
+            class="offer-table-item__relation-button--sublease"
         >
-            <i class="fa-solid fa-caret-up"></i>
-            <span>Субаренда</span>
-            <i
-                class="fa-solid offer-table-item__relation-icon"
-                :class="{
-                    'fa-eye-slash': currentTab === dealOptions.typeStatement.SUBLEASE,
-                    'fa-eye': currentTab !== dealOptions.typeStatement.SUBLEASE
-                }"
-            />
-        </button>
+            Субаренда
+        </OfferTableItemRelationSelectElement>
     </div>
 </template>
 
 <script setup>
 import { dealOptions } from '@/const/options/deal.options.js';
 import { computed } from 'vue';
+import OfferTableItemRelationSelectElement from '@/components/Offer/TableItem/OfferTableItemRelationSelectElement.vue';
 
 defineEmits(['open']);
 const props = defineProps({
@@ -115,28 +64,22 @@ const props = defineProps({
     }
 });
 
+const offerExistByType = (offers, type) => {
+    return offers.some(
+        element => element.deal_type === type && element.id !== props.current && !element.is_deleted
+    );
+};
+
 const hasRentOffers = computed(() =>
-    props.offers.some(
-        element =>
-            element.deal_type === dealOptions.typeStatement.RENT && element.id !== props.current
-    )
+    offerExistByType(props.offers, dealOptions.typeStatement.RENT)
 );
 const hasSaleOffers = computed(() =>
-    props.offers.some(
-        element =>
-            element.deal_type === dealOptions.typeStatement.SALE && element.id !== props.current
-    )
+    offerExistByType(props.offers, dealOptions.typeStatement.SALE)
 );
 const hasStorageOffers = computed(() =>
-    props.offers.some(
-        element =>
-            element.deal_type === dealOptions.typeStatement.STORAGE && element.id !== props.current
-    )
+    offerExistByType(props.offers, dealOptions.typeStatement.STORAGE)
 );
 const hasSubleaseOffers = computed(() =>
-    props.offers.some(
-        element =>
-            element.deal_type === dealOptions.typeStatement.SUBLEASE && element.id !== props.current
-    )
+    offerExistByType(props.offers, dealOptions.typeStatement.SUBLEASE)
 );
 </script>
