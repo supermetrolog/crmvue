238aa650d944050ede8e01d36a8c6cb27f202946
nknwns <andrejshaev@gmail.com>
2024-12-28 00:52:17 +0700

    feat: improve request requirements view

---
+++ src/components/Request/RequestPreviewRequirementsSection.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p class="request-preview__subtitle mb-1">{{ label }}</p>
+        <div class="request-preview__grid">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script setup>
+defineProps({
+    label: String
+});
+</script>
