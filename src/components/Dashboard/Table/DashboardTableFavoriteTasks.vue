022f55ef60354340bff5c1bfc90a3271f08f7572
nknwns <andrejshaev@gmail.com>
2025-01-07 00:47:46 +0700

    feat: drag and drop sorting favorite tasks

--- src/components/Dashboard/Table/DashboardTableFavoriteTasks.vue
+++ src/components/Dashboard/Table/DashboardTableFavoriteTasks.vue
@@ -4,35 +4,40 @@
             <i class="fa-solid fa-star"></i>
             <span>Избранный задачи</span>
         </DashboardChip>
-        <div class="dashboard-tasks-table">
-            <DashboardTableTasksItem
-                v-for="task in tasks"
-                :key="task.id"
-                @view="openPreviewer(task, $event)"
-                :task="task"
-            />
-            <EmptyData v-if="!tasks.length">Список избранного пуст..</EmptyData>
-            <Modal @close="previewIsVisible = false" :show="previewIsVisible">
-                <template #container>
-                    <div v-if="previewIsLoading" class="dashboard-tasks-table__card">
-                        <Spinner label="Загрузка задачи.." class="absolute-center" />
-                    </div>
-                    <TaskCard
-                        v-else
-                        @read="onRead"
-                        @to-chat="toChat"
-                        @updated="onUpdated"
-                        @deleted-comment="onDeletedComment"
-                        @added-comment="onAddedComment"
-                        @history-changed="onChangedHistory"
-                        class="dashboard-tasks-table__card"
-                        :task="currentTask"
-                        :draggable="userCanDrag"
-                        :editable="userCanEdit"
-                    />
-                </template>
-            </Modal>
-        </div>
+        <VirtualDragList
+            v-model="currentTasks"
+            group="tasks"
+            data-key="id"
+            :keeps="40"
+            chosenClass="chosen"
+            wrap-class="dashboard-tasks-table"
+            lockAxis="x"
+        >
+            <template #item="{ record }">
+                <DashboardTableTasksItem @view="openPreviewer(record, $event)" :task="record" />
+            </template>
+        </VirtualDragList>
+        <EmptyData v-if="!tasks.length">Список избранного пуст..</EmptyData>
+        <Modal @close="previewIsVisible = false" :show="previewIsVisible">
+            <template #container>
+                <div v-if="previewIsLoading" class="dashboard-tasks-table__card">
+                    <Spinner label="Загрузка задачи.." class="absolute-center" />
+                </div>
+                <TaskCard
+                    v-else
+                    @read="onRead"
+                    @to-chat="toChat"
+                    @updated="onUpdated"
+                    @deleted-comment="onDeletedComment"
+                    @added-comment="onAddedComment"
+                    @history-changed="onChangedHistory"
+                    class="dashboard-tasks-table__card"
+                    :task="currentTask"
+                    :draggable="userCanDrag"
+                    :editable="userCanEdit"
+                />
+            </template>
+        </Modal>
     </div>
 </template>
 
@@ -49,14 +54,17 @@ import { toDateFormat } from '@/utils/formatters/date.js';
 import { spliceById } from '@/utils/helpers/array/spliceById.js';
 import DashboardChip from '@/components/Dashboard/DashboardChip.vue';
 import EmptyData from '@/components/common/EmptyData.vue';
+import VirtualDragList from 'vue-virtual-draglist';
 
 const emit = defineEmits(['task-updated', 'hide']);
-defineProps({
+const props = defineProps({
     tasks: {
         type: Array,
         default: () => []
     }
 });
+
+const currentTasks = ref([...props.tasks]);
 
 const { openMessenger } = useMessenger();
 const { currentUserId, currentUserIsModerator } = useAuth();
