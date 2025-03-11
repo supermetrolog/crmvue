5e3f825a098527744f3fcd09de9bc98ebe54b764
nknwns <andrejshaev@gmail.com>
2025-03-12 06:19:17 +0700

    fix: fix Messenger quiz custom templates

--- src/components/SurveyCard/SurveyCardQuestion.vue
+++ src/components/SurveyCard/SurveyCardQuestion.vue
@@ -1,17 +1,17 @@
 <template>
     <component :is="currentComponent" :question="question">
         <template v-if="isQuestionWithCompaniesInfo" #textarea="{ answers }">
-            <SurveyCardQuestionTemplateCompany :answers="answers" />
+            <SurveyCardQuestionTemplateCustomCompany :answers="answers" />
         </template>
         <template v-else-if="isQuestionWithRelevantRequestsInfo" #textarea="{ answers }">
-            <SurveyCardQuestionTemplateRequestsList :answers="answers" />
+            <SurveyCardQuestionTemplateCustomRequestsList :answers="answers" />
         </template>
     </component>
 </template>
 <script setup>
 import { computed } from 'vue';
-import SurveyCardQuestionTemplateCompany from '@/components/SurveyCard/QuestionTemplate/SurveyCardQuestionTemplateCompany.vue';
-import SurveyCardQuestionTemplateRequestsList from '@/components/SurveyCard/QuestionTemplate/SurveyCardQuestionTemplateRequestsList.vue';
+import SurveyCardQuestionTemplateCustomCompany from '@/components/SurveyCard/QuestionTemplate/Custom/SurveyCardQuestionTemplateCustomCompany.vue';
+import SurveyCardQuestionTemplateCustomRequestsList from '@/components/SurveyCard/QuestionTemplate/Custom/SurveyCardQuestionTemplateCustomRequestsList.vue';
 import { quizEffectKinds } from '@/const/quiz.js';
 import SurveyCardQuestionTemplateWantsToSell from '@/components/SurveyCard/QuestionTemplate/SurveyCardQuestionTemplateWantsToSell.vue';
 import SurveyCardQuestionTemplateFreeArea from '@/components/SurveyCard/QuestionTemplate/SurveyCardQuestionTemplateFreeArea.vue';
