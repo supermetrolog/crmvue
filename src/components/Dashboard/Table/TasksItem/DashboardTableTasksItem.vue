0f679f3b436a58b11eac38bfdf9e3d2a6b28dddc
nknwns <andrejshaev@gmail.com>
2025-04-01 15:35:29 +0700

    fix: change Task `expired` checking

--- src/components/Dashboard/Table/TasksItem/DashboardTableTasksItem.vue
+++ src/components/Dashboard/Table/TasksItem/DashboardTableTasksItem.vue
@@ -2,7 +2,7 @@
     <div
         class="dashboard-card-task"
         :class="{
-            expired: isAlreadyExpired && !isCanceled && isForMe,
+            expired: isAlreadyExpired && !isCanceled,
             completed: isCompleted,
             self: isForMe,
             myself: isMyTask,
