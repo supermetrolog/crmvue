7cd57ac42fc39157e544aafd93c53991f3e93f64
nknwns <andrejshaev@gmail.com>
2025-04-14 00:30:22 +0700

    feat: add purposes picker to utility form

--- src/views/Settings/Utilities.vue
+++ src/views/Settings/Utilities.vue
@@ -1,14 +1,41 @@
 <template>
     <div class="container-fluid">
+        <div class="row">
+            <UiCol :cols="12">
+                <DashboardCard title="Инструменты модератора">
+                    <div class="row">
+                        <UiCol :cols="4">
+                            <DashboardCard>
+                                <p class="mb-1 font-weight-semi">Исправить назначения участка</p>
+                                <p class="text-grey">Описание:</p>
+                                <p>
+                                    Инструмент для исправления объектов, у которых не отображается
+                                    выбор назначения.
+                                </p>
+                                <UiButton
+                                    @click="purposesFixFormIsVisible = true"
+                                    small
+                                    class="mt-2"
+                                    color="light"
+                                >
+                                    Использовать
+                                </UiButton>
+                            </DashboardCard>
+                        </UiCol>
+                    </div>
+                </DashboardCard>
+            </UiCol>
+        </div>
         <Teleport to="body">
             <UiModal
                 v-model:visible="purposesFixFormIsVisible"
                 @closed="clearPurposesFixForm"
                 :width="600"
-                title="Object Land Purposes Fix"
+                title="Исправить назначения участка"
             >
                 <MultiSelect
                     v-model="purposesFixForm.object_id"
+                    @change="onChange"
                     :disabled="purposesFixIsRunning"
                     label="ID объекта"
                     searchable
@@ -29,14 +56,14 @@
                                 <p>
                                     Объект #{{ option.value }},
                                     <span :class="{ 'text-grey': !isSelected }">
-                                        {{ option.is_land ? 'участок' : 'строение' }}</span
-                                    >
+                                        {{ option.is_land ? 'участок' : 'строение' }}
+                                    </span>
                                 </p>
                                 <p class="fs-2" :class="{ 'text-grey': !isSelected }">
                                     {{ option.address }}
                                 </p>
                                 <p class="fs-2" :class="{ 'text-primary': !isSelected }">
-                                    purposes: {{ option.purposes ?? '-' }}
+                                    Назначения: {{ option.purposes ?? '-' }}
                                 </p>
                             </div>
                         </div>
@@ -46,55 +73,60 @@
                             <p>
                                 Объект #{{ option.value }},
                                 <span class="text-grey">
-                                    {{ option.is_land ? 'участок' : 'строение' }}</span
-                                >
+                                    {{ option.is_land ? 'участок' : 'строение' }}
+                                </span>
                             </p>
                             <p class="text-grey fs-2">{{ option.address }}</p>
                         </div>
                     </template>
                 </MultiSelect>
+                <div class="mt-2">
+                    <p class="font-weight-semi">Назначения:</p>
+                    <ObjectTypePicker
+                        v-model:value="purposesFixForm.purposes"
+                        :options="objectPurposesWithSectionsOptions.plot"
+                    />
+                </div>
+                <div class="d-flex mt-2 gap-2">
+                    <UiButton @click="selectAll" icon="fa-solid fa-check" color="light" small>
+                        Выбрать все
+                    </UiButton>
+                    <UiButton
+                        @click="clearAll"
+                        icon="fa-solid fa-xmark"
+                        color="light"
+                        small
+                        :disabled="!purposesFixForm.purposes.length"
+                    >
+                        Сбросить
+                    </UiButton>
+                </div>
                 <template #actions="{ close }">
                     <UiButton
                         @click="runPurposesFix"
-                        :disabled="!purposesFixForm.object_id"
+                        :disabled="!canBeFixed"
                         :loading="purposesFixIsRunning"
-                        color="success"
+                        icon="fa-solid fa-check"
+                        color="success-light"
                     >
                         Выполнить
                     </UiButton>
-                    <UiButton @click="close" :disabled="purposesFixIsRunning" color="danger">
+                    <UiButton
+                        @click="close"
+                        :disabled="purposesFixIsRunning"
+                        icon="fa-solid fa-ban"
+                        color="light"
+                    >
                         Отмена
                     </UiButton>
                 </template>
             </UiModal>
         </Teleport>
-        <div class="row">
-            <div class="col-12">
-                <DashboardCard title="Инструменты администратора">
-                    <div class="row">
-                        <UiCol cols="3">
-                            <DashboardCard>
-                                <p class="mb-1">Object Land Purposes Fix</p>
-                                <p class="text-grey">Описание:</p>
-                                <p>Исправить назначения объекта (участка)</p>
-                                <UiButton
-                                    @click="purposesFixFormIsVisible = true"
-                                    small
-                                    class="mt-2"
-                                >
-                                    Использовать
-                                </UiButton>
-                            </DashboardCard>
-                        </UiCol>
-                    </div>
-                </DashboardCard>
-            </div>
-        </div>
     </div>
 </template>
 <script setup>
 import DashboardCard from '@/components/Dashboard/Card/DashboardCard.vue';
-import { reactive, ref, toRaw } from 'vue';
+import { computed, reactive, ref, shallowRef, toRaw } from 'vue';
 import api from '@/api/api.js';
 import { useNotify } from '@/utils/use/useNotify.js';
 import { useConfirm } from '@/composables/useConfirm.js';
@@ -103,6 +135,9 @@ import UiButton from '@/components/common/UI/UiButton.vue';
 import UiModal from '@/components/common/UI/UiModal.vue';
 import MultiSelect from '@/components/common/Forms/MultiSelect.vue';
 import LazyImage from '@/components/common/LazyImage.vue';
+import ObjectTypePicker from '@/components/common/Forms/ObjectTypePicker.vue';
+import { objectPurposesWithSectionsOptions } from '@/const/options/object.options.js';
+import { isNullish } from '@/utils/helpers/common/isNullish.js';
 
 const notify = useNotify();
 const { confirm } = useConfirm();
@@ -111,12 +146,28 @@ const purposesFixFormIsVisible = ref(false);
 
 const purposesFixIsRunning = ref(false);
 const purposesFixForm = reactive({
-    object_id: null
+    object_id: null,
+    purposes: []
+});
+
+const selectedObject = shallowRef(null);
+
+function onChange(_, option) {
+    selectedObject.value = option;
+}
+
+const canBeFixed = computed(() => {
+    if (isNullish(purposesFixForm.object_id)) return false;
+    if (isNullish(selectedObject.value)) return false;
+    if (purposesFixForm.purposes.length === 0) return false;
+
+    return selectedObject.value.purposes.length === 0;
 });
 
 async function getObjectsOptions(querySearch) {
     let query = {
-        expand: null
+        expand: null,
+        is_land: 1
     };
 
     if (querySearch == null) {
@@ -138,6 +189,8 @@ async function getObjectsOptions(querySearch) {
 
 function clearPurposesFixForm() {
     purposesFixForm.object_id = null;
+    purposesFixForm.purposes = [];
+    selectedObject.value = null;
 }
 
 async function runPurposesFix() {
@@ -155,5 +208,15 @@ async function runPurposesFix() {
     } finally {
         purposesFixIsRunning.value = false;
     }
+}
+
+// purposes
+
+function selectAll() {
+    purposesFixForm.purposes = Object.keys(objectPurposesWithSectionsOptions.plot);
+}
+
+function clearAll() {
+    purposesFixForm.purposes = [];
 }
 </script>
