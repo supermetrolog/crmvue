2c5007cc19853b8a1b147901a49942cc2cfcc1ec
nknwns <andrejshaev@gmail.com>
2025-05-25 04:49:24 +0700

    api: add `surveyDraft` endpoint

---
+++ src/api/surveyDraft.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { responseToData } from '@/api/helpers/responseToData.js';
+import { responseToPaginatedData } from '@/api/helpers/responseToPaginatedData.js';
+import { responseHasStatus } from '@/api/helpers/responseHasStatus.js';
+import { STATUS_SUCCESS } from '@/api/helpers/statuses.js';
+
+const URL = '/survey-drafts';
+
+export default {
+    async create(options) {
+        const response = await axios.postForm(URL, options);
+        return responseToData(response);
+    },
+    async get(id) {
+        const response = await axios.get(`${URL}/${id}`);
+        return responseToData(response);
+    },
+    async list(params) {
+        const response = await axios.get(URL, { params });
+        return responseToPaginatedData(response);
+    },
+    async delete(id) {
+        const response = await axios.delete(`${URL}/${id}`);
+        return responseHasStatus(response.status, STATUS_SUCCESS);
+    },
+    async update(id, payload) {
+        const response = await axios.putForm(`${URL}/${id}`, payload);
+        return responseToData(response);
+    },
+    async find(params) {
+        const response = await axios.get(URL, { params });
+        return responseToData(response)[0] ?? null;
+    },
+    async getByChatMemberId(chatMemberId) {
+        const response = await axios.get(`${URL}/chat-members/${chatMemberId}`);
+        return responseToData(response);
+    }
+};
