d40db8b724d86be1ab291de54fdce716049d6890
nknwns <andrejshaev@gmail.com>
2025-11-24 16:31:51 +0700

    feat(survey): more info about offer area

---
+++ src/components/SurveyForm/ObjectsPreview/SurveyFormObjectsPreviewOfferAreaRow.vue
@@ -0,0 +1,31 @@
+<template>
+    <p v-if="hasValue" class="area-row">
+        <with-unit-type :unit-type="unitTypes.SQUARE_METERS">
+            {{ formattedValue }}
+        </with-unit-type>
+        <span> - {{ label }}</span>
+    </p>
+</template>
+<script setup lang="ts">
+import WithUnitType from '@/components/common/WithUnitType.vue';
+import { unitTypes } from '@/const/unitTypes.js';
+import { computed } from 'vue';
+import { toNumberOrRangeFormat } from '@/utils/formatters/number';
+
+const props = defineProps<{
+    areaMin: number;
+    areaMax: number;
+    label: string;
+}>();
+
+const hasValue = computed(() => props.areaMin || props.areaMax);
+
+const formattedValue = computed(() => toNumberOrRangeFormat(props.areaMin, props.areaMax));
+</script>
+
+<style scoped>
+.area-row {
+    display: inline-block;
+    line-height: 110%;
+}
+</style>
