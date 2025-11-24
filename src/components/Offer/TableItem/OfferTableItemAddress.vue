627ec45ce5a8b36206f105fee738c44c4c2f885d
nknwns <andrejshaev@gmail.com>
2025-11-24 16:58:54 +0700

    feat(offers): copy address

--- src/components/Offer/TableItem/OfferTableItemAddress.vue
+++ src/components/Offer/TableItem/OfferTableItemAddress.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :href target="_blank">
+    <a :href target="_blank" class="offer-table-item-address">
         <p v-if="offer.region_name" class="font-weight-bold">
             {{ offer.region_name }}
         </p>
@@ -21,12 +21,27 @@
         <p v-if="offer.address">
             {{ offer.address }}
         </p>
+        <UiButton
+            v-if="hasAddress"
+            @click.stop.prevent="copyAddress"
+            mini
+            color="light"
+            :icon="alreadyCopied ? 'fa-solid fa-check' : 'fa-solid fa-copy'"
+            :disabled="alreadyCopied"
+            class="offer-table-item-address__copy"
+        >
+            {{ alreadyCopied ? 'Скопировано' : 'Копировать адрес' }}
+        </UiButton>
     </a>
 </template>
 
 <script setup>
 import { computed } from 'vue';
 import { getLinkOfferByObject } from '@/utils/url.js';
+import UiButton from '@/components/common/UI/UiButton.vue';
+import { useClipboard } from '@vueuse/core';
+import { useNotify } from '@/utils/use/useNotify';
+import { isNotNullish } from '@/utils/helpers/common/isNotNullish';
 
 const props = defineProps({
     offer: {
@@ -36,4 +51,22 @@ const props = defineProps({
 });
 
 const href = computed(() => getLinkOfferByObject(props.offer));
+
+// copy
+
+const { copy, text: clipboardText } = useClipboard();
+const notify = useNotify();
+
+const hasAddress = computed(
+    () => isNotNullish(props.offer.address) && props.offer.address.length > 0
+);
+
+const alreadyCopied = computed(
+    () => hasAddress.value && props.offer.address === clipboardText.value
+);
+
+function copyAddress() {
+    copy(props.offer.address);
+    notify.info('Адрес скопирован в буфер обмена');
+}
 </script>
